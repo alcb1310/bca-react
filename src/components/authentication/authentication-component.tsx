30583ab9607729907c3a9b732809b20477c979e0
Andrés Court <andres@andrescourt.com>
2023-08-08 22:35:10 -0500

    TODO: change the testing console log

--- src/components/authentication/authentication-component.tsx
+++ src/components/authentication/authentication-component.tsx
@@ -1,26 +1,20 @@
 import { useCookies } from 'react-cookie'
 import { useValidateQuery } from '../../api/login/bca-api-slice'
-import { useAppSelector, useAppDispatch } from '../../store/hooks'
+import { useAppSelector } from '../../store/hooks'
 import { Navigate, Outlet, useLocation } from "react-router-dom"
-import { logOut } from '../../store/login/loginSlice'
 
 export default function Authenticated() {
     const isAuthenticated = useAppSelector(state => state.login.isLoggedIn)
-    const dispatch = useAppDispatch()
     const [cookies, setCookie] = useCookies(["BCAAuth"])
     const validate = useValidateQuery(1)
     const location = useLocation()
-    // console.log(cookies)
 
-    console.log("validate", validate)
+    // TODO: change the console log
     if (!validate.isLoading) {
-        console.log("validate", validate)
         if (validate.isError) {
-            dispatch(logOut())
-            return <Navigate to="/login" state={{ from: location }} replace />
+            console.log({ validate })
         }
         let savedJWT = cookies.BCAAuth
-        console.log(savedJWT)
 
         if (validate.data?.jwt !== cookies.BCAAuth) {
             savedJWT = validate.data?.jwt ? validate.data.jwt : ""
@@ -28,7 +22,6 @@ export default function Authenticated() {
         }
 
     }
-    console.log(isAuthenticated)
 
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />
 }
