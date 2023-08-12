a62b5eb7703e64ff3db6dc56b70ff3c29a2124e7
Andrés Court <andres@andrescourt.com>
2023-08-12 16:14:25 -0500

    fail test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -10,17 +10,18 @@ describe("Login to the app", () => {
         cy.get('[data-testid="login.password"]').type("password123")
         cy.get('[data-testid="login.button"]').click()
 
-        cy.get('[data-testid="landing.title"]').should('exist')
 
         cy.get('[data-testid="site.title"]').should('have.text', "Sistema de control presupuestario")
         cy.get('[data-testid="site.logout"]').should('exist')
         cy.get('[data-testid="site.logout"]').click()
 
+        cy.get('[data-testid="landing.title"]').should('exist')
         cy.get('[data-testid="login.title"]', { timeout: 10000 }).should('exist')
         cy.get('[data-testid="login.title"]').should('have.text', "Login")
     })
 
     it("should go to the caller page when login", () => {
         cy.visit("/settings/suppliers")
+
     })
 })
