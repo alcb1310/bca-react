686a2151693794191806cf6c9ce8eb88eaf0c1d7
Andrés Court <andres@andrescourt.com>
2023-08-12 12:13:57 -0500

    added failing test

--- cypress/e2e/first.cy.ts
+++ cypress/e2e/first.cy.ts
@@ -2,4 +2,8 @@ describe("My first test", () => {
     it("Does not do much", () => {
         expect(true).to.equal(true)
     })
+
+    it("should fail and not let ci/cd pr merge", () => {
+        expect(true).to.equal(false)
+    })
 })
