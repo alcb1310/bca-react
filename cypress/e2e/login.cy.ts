20e22e0c7b3dc840643c6340103c27608cfb1980
Andrés Court <andres@andrescourt.com>
2023-08-12 17:40:03 -0500

    Should be able to login and logout

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-    it('passes', () => {
+describe('Login test', () => {
+    it('Should be able to login and logout', () => {
         cy.visit('/')
 
         cy.get('[data-testid="login.title"]').should('exist')
@@ -8,5 +8,13 @@ describe('template spec', () => {
         cy.get('[data-testid="login.email"]').type('andres@andrescourt.com')
         cy.get('[data-testid="login.password"]').type('password123')
         cy.get('[data-testid="login.button"]').click()
+
+        cy.get('[data-testid="landing.title"]').should("have.text", "Bienvenido")
+
+        cy.get('[data-testid="titlebar.title"]').should("have.text", "Sistema de control presupuestario")
+        cy.get('[data-testid="titlebar.logout"]').should('exist').should('have.text', "Salir")
+        cy.get('[data-testid="titlebar.logout"]').click()
+
+        cy.get('[data-testid="login.title"]').should('have.text', "Login")
     })
 })
