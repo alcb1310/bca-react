42edb52e8310fcd12ca59e69e0ea261fb3b7a201
Andrés Court <andres@andrescourt.com>
2023-08-12 17:44:40 -0500

    Debug 2

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,9 +5,9 @@ describe('Login test', () => {
         cy.get('[data-testid="login.title"]').should('exist')
         cy.get('[data-testid="login.title"]').should('have.text', "Login")
 
-        // cy.get('[data-testid="login.email"]').type('andres@andrescourt.com')
-        // cy.get('[data-testid="login.password"]').type('password123')
-        // cy.get('[data-testid="login.button"]').click()
+        cy.get('[data-testid="login.email"]').type('andres@andrescourt.com')
+        cy.get('[data-testid="login.password"]').type('password123')
+        cy.get('[data-testid="login.button"]').click()
         //
         // cy.get('[data-testid="landing.title"]').should("have.text", "Bienvenido")
         //
