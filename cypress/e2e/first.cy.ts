543ac2d5c76f39c8e8ef2070b81cd6fa290f909a
Andrés Court <andres@andrescourt.com>
2023-08-12 12:06:01 -0500

    Remove failing test

--- cypress/e2e/first.cy.ts
+++ cypress/e2e/first.cy.ts
@@ -2,8 +2,4 @@ describe("My first test", () => {
     it("Does not do much", () => {
         expect(true).to.equal(true)
     })
-
-    it("should fail and not let ci/cd pr merge", () => {
-        expect(true).to.equal(false)
-    })
 })
