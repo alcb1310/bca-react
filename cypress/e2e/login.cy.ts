9d2d0959e252b1678a7a9f3e6f1124ef169087c0
Andrés Court <andres@andrescourt.com>
2023-08-12 17:58:44 -0500

    Final login test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -10,12 +10,12 @@ describe('Login test', () => {
         cy.get('[data-testid="login.button"]').should('exist')
         cy.get('[data-testid="login.button"]').click()
 
-        // cy.get('[data-testid="landing.title"]').should("have.text", "Bienvenido")
-        //
-        // cy.get('[data-testid="titlebar.title"]').should("have.text", "Sistema de control presupuestario")
-        // cy.get('[data-testid="titlebar.logout"]').should('exist').should('have.text', "Salir")
-        // cy.get('[data-testid="titlebar.logout"]').click()
-        //
-        // cy.get('[data-testid="login.title"]').should('have.text', "Login")
+        cy.get('[data-testid="landing.title"]').should("have.text", "Bienvenido")
+
+        cy.get('[data-testid="titlebar.title"]').should("have.text", "Sistema de control presupuestario")
+        cy.get('[data-testid="titlebar.logout"]').should('exist').should('have.text', "Salir")
+        cy.get('[data-testid="titlebar.logout"]').click()
+
+        cy.get('[data-testid="login.title"]').should('have.text', "Login")
     })
 })
