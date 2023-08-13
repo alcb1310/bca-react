11b0ec8e55e5a2faed359fe8c08c894463c3b205
Andrés Court <andres@andrescourt.com>
2023-08-12 23:43:56 -0500

    added more controls to work with msw

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 import "./i18n"
-const dev = import.meta.env.NODE_ENV as string
+let dev = import.meta.env.NODE_ENV as string
 
 async function dynImports() {
-    console.log(dev)
+    dev = dev ? dev : "test"
     if (dev === 'development' || dev === 'test') {
         console.log(dev)
         const { worker } = await import('./mocks/browser')
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,16 @@
 // src/mocks/handlers.js
 import { rest } from 'msw'
 import { login } from './bca/fixtures/login'
+import { validate } from './bca/fixtures/validate'
 
 // const url = import.meta.env.VITE.BASE_URL as string
 export const handlers = [
     // Handles a POST /login request
     rest.post('/api/login', (_req, res, ctx) => {
-        sessionStorage.setItem('is-authenticated', 'true')
-
         return res(ctx.status(200), ctx.json(login))
     }),
+
+    rest.get("/api/validate", (_req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(validate))
+    })
 ]
