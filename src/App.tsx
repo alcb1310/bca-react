1af75b3ab4078bf7ee07182604ca9ef72dc3ec2f
Andres Court <andres@andrescourt.com>
2024-07-20 22:31:16 -0500

    chore: Material-UI and Material-UI-Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { SettingsOutlined } from "@mui/icons-material";
+import { Button } from "@mui/material";
+
 function App() {
   return (
     <>
             <h1 className="text-3xl font-bold underline">Hello World!</h1>
+            <Button variant="contained" endIcon={<SettingsOutlined />} size="small">Hello World</Button>
     </>
   );
 }
