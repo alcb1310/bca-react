09c857fe30f714dbc579cc31cd9259500038a20d
Andres Court <andres@andrescourt.com>
2024-07-21 21:28:09 -0500

    chore: Add material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { SettingsOutlined } from "@mui/icons-material";
+import { Button } from "@mui/material";
+
 export default function App() {
   return (
     <>
-            <h1 className="text-green-800 text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-green-800 text-3xl font-bold">Hello world!</h1>
+      <Button
+        variant="contained"
+        endIcon={<SettingsOutlined />}
+        size="small"
+        color="success"
+      >
+        Contained
+      </Button>
     </>
   );
 }
