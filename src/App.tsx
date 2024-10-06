322bdc79433c2bcb5e6bde4712acd5651957916e
Andres Court <andres@andrescourt.com>
2024-10-05 21:36:45 -0500

    chore: Add project's colors to the tailwind theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@mui/material"
+import { Button, Typography } from "@mui/material"
 
 function App() {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <Button variant="contained" color="primary">Hello World</Button>
+
+      <Typography variant="h1">Hello World</Typography>
     </div>
   )
 }
