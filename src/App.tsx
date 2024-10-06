03900872e56a4ce581cdfcf0e9fb7dae5ba4c6b0
Andres Court <andres@andrescourt.com>
2024-10-06 00:33:49 -0500

    chore: Installed redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import { Button, Typography } from "@mui/material"
+import { incrementBy } from "./redux/features/counter/counterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hooks"
+import { useFetchBreedsQuery } from "./redux/features/dogs/dogs-api-slice"
 
 function App() {
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  const { data = [], isFetching, isError } = useFetchBreedsQuery()
+  if (isError) {
+    return <div>Error</div>
+  }
+
+  if (isFetching) {
+    return <div>Loading...</div>
+  }
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <Button variant="contained" color="primary">Hello World</Button>
+    <div className="w-screen h-screen">
+      <Button
+        onClick={() => dispatch(incrementBy(10))}
+        variant="contained" color="primary"
+      >count is: {count}</Button>
 
       <Typography variant="h1">Hello World</Typography>
+
+      <Typography variant="body1">Number of dogs fetched: {data.length}</Typography>
+
+      <table>
+        <thead></thead>
+        <tbody>
+          {data.map((breed) => (
+            <tr key={breed.id}>
+              <td>{breed.name}</td>
+              {breed.image && <td><img src={breed.image.url} alt={breed.name} /></td>}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
