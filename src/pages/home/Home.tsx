857b82170079cad72e37c8ac81b4ddf18dce54be
Andres Court <andres@andrescourt.com>
2024-10-09 16:30:46 -0500

    feat: Application's landing page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,26 @@
-import { Typography } from "@mui/material";
+import { CircularProgress, Typography } from "@mui/material";
+import { useMeQuery } from "../../redux/api/bca-backend/user/userSlice";
 
 export default function Home() {
+  const { data, isLoading } = useMeQuery()
+
+
   return (
     <>
-      <Typography variant="h5" component="h5" textTransform="uppercase" >
-        Home
-      </Typography>
+      {
+        isLoading ?
+          <CircularProgress /> :
+          <Typography variant="body1" component="p">
+            Bienvenido&nbsp;
+            <Typography
+              variant="body1"
+              component="span"
+              color="success"
+            >
+              {data?.name}
+            </Typography>
+          </Typography>
+      }
     </>
   )
 }
