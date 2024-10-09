c23e20980b5920abf11dab0a61fba2fe90ade66b
Andres Court <andres@andrescourt.com>
2024-10-08 23:14:36 -0500

    feat: save the user and the token in the store

--- src/redux/features/login/loginSlice.ts
+++ src/redux/features/login/loginSlice.ts
@@ -1,23 +1,31 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { UserResponse } from "../../../types/user"
 
+type PayloadResponse = {
+  user: UserResponse
+  token: string
+}
+
 type LoginType = {
   isLoggedIn: boolean
   loggedInUser: UserResponse | null
+  token: string
 }
 
 const initialState: LoginType = {
   isLoggedIn: false,
-  loggedInUser: null
+  loggedInUser: null,
+  token: '',
 }
 
 export const loginSlice = createSlice({
   name: "login",
   initialState,
   reducers: {
-    login: (state, user: PayloadAction<UserResponse>) => {
+    login: (state, user: PayloadAction<PayloadResponse>) => {
       state.isLoggedIn = true
-      state.loggedInUser = user.payload
+      state.loggedInUser = user.payload.user
+      state.token = user.payload.token
     },
 
     logout: (state) => {
