d26389c117a87fdf9ff45b66e17407f2e6bea028
Andres Court <andres@andrescourt.com>
2024-10-09 22:41:35 -0500

    feat: Display required buttons in top of the table

--- src/pages/users/admin/index.tsx
+++ src/pages/users/admin/index.tsx
@@ -1,10 +1,17 @@
+import EditToolbar from "../../../components/table/headers/toolbar";
 import PageTitle from "../../../components/titles/PageTitle";
 import AllUsersTable from "../../../components/users/AllUsersTable";
 
 export default function Admin() {
+  function handleClick() {
+    console.log("clicked")
+  }
+
   return (
     <>
       <PageTitle title="Administración de usuarios" />
+
+      <EditToolbar title="Crear Usuario" onClick={handleClick} />
       <AllUsersTable />
     </>
   )
