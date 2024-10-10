5e2d67d7e7e1e9a5985c1dc6f35f11068541cc91
Andres Court <andres@andrescourt.com>
2024-10-09 22:41:03 -0500

    feat: Display users's information in a table

---
+++ src/components/users/AllUsersTable.tsx
@@ -0,0 +1,55 @@
+import { DeleteOutline, EditOutlined } from "@mui/icons-material";
+import { useAllUsersQuery, useMeQuery } from "../../redux/api/bca-backend/user/userSlice"
+import { DataGrid, GridColDef, GridActionsCellItem } from '@mui/x-data-grid';
+
+export default function AllUsersTable() {
+  const { data, isLoading } = useAllUsersQuery()
+  const { data: me } = useMeQuery()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  const cols: GridColDef[] = [
+    { field: "name", headerName: "Name", width: 300 },
+    { field: "email", headerName: "Email", width: 300 },
+    {
+      field: "actions",
+      type: "actions",
+      width: 80,
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon=<EditOutlined color="warning" />
+          label="Edit"
+          onClick={() => {
+            console.log(params)
+          }}
+        />,
+
+        <GridActionsCellItem
+          disabled={me?.id === params.row.id}
+          icon=<DeleteOutline className={me?.id === params.row.id ? "text-red-200" : "text-red-500"} />
+          label="Delete"
+          onClick={() => {
+            console.log(params)
+          }}
+        />,
+      ]
+    },
+  ]
+
+  return (
+    <>
+      <DataGrid
+        rows={data || []}
+        columns={cols}
+        sx={{ '&, [class^=MuiDataGrid]': { border: 'none' } }}
+        disableColumnFilter
+        disableColumnResize
+        hideFooterPagination={true}
+        disableRowSelectionOnClick
+        disableMultipleRowSelection
+      />
+    </>
+  )
+}
--- src/pages/users/admin/index.tsx
+++ src/pages/users/admin/index.tsx
@@ -1,9 +1,11 @@
 import PageTitle from "../../../components/titles/PageTitle";
+import AllUsersTable from "../../../components/users/AllUsersTable";
 
 export default function Admin() {
   return (
     <>
       <PageTitle title="Administración de usuarios" />
+      <AllUsersTable />
     </>
   )
 }
