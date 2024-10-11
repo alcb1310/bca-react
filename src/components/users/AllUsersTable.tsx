d214092d624abf6ed9ac46015bae3e450db4f95e
Andres Court <andres@andrescourt.com>
2024-10-10 22:25:22 -0500

    feat: Confirmation message to delete a user

--- src/components/users/AllUsersTable.tsx
+++ src/components/users/AllUsersTable.tsx
@@ -1,40 +1,52 @@
+import { useState } from "react";
 import { DeleteOutline, EditOutlined } from "@mui/icons-material";
 import { DataGrid, GridColDef, GridActionsCellItem } from '@mui/x-data-grid';
 
 import { useAllUsersQuery, useDeleteUserMutation, useMeQuery } from "../../redux/api/bca-backend/user/userSlice"
-
-// TODO: Add confirmation message when deleting a user
+import ConfirmationDialog from "../dialog/ConfirmationDialog";
 
 export default function AllUsersTable() {
+  const [confirmationDialogOpen, setConfirmationDialogOpen] = useState<boolean>(false)
+  const [userIdToDelete, setUserIdToDelete] = useState<string>("")
+  const [message, setMessage] = useState<string>("")
+
   const { data, isLoading } = useAllUsersQuery()
   const { data: me } = useMeQuery()
   const [deleteUser] = useDeleteUserMutation()
+
+  function setOpenConfirmationDialog(open: boolean, id: string, name: string) {
+    setUserIdToDelete(id)
+    setConfirmationDialogOpen(open)
+    setMessage(`Seguro que quieres borrar al usuario ${name}?`)
+  }
 
   if (isLoading) {
     return <div>Loading...</div>
   }
 
   const cols: GridColDef[] = [
-    { field: "name", headerName: "Name", width: 300 },
-    { field: "email", headerName: "Email", width: 300 },
+    { field: "name", headerName: "Name", width: 300, resizable: true },
+    { field: "email", headerName: "Email", width: 300, resizable: false },
     {
       field: "actions",
       type: "actions",
-      width: 80,
+      width: 10,
+      resizable: false,
       getActions: (params) => [
         <GridActionsCellItem
           icon=<EditOutlined color="warning" />
+          showInMenu
           label="Edit"
-          onClick={() => {
-            console.log(params)
-          }}
+          onClick={() => { console.log(params.row) }}
         />,
+
 
         <GridActionsCellItem
           disabled={me?.id === params.row.id}
           icon=<DeleteOutline className={me?.id === params.row.id ? "text-red-200" : "text-red-500"} />
           label="Delete"
-          onClick={() => { deleteUser(params.row.id) }}
+          showInMenu
+          onClick={() => { setOpenConfirmationDialog(true, params.row.id, params.row.name) }}
         />,
       ]
     },
@@ -53,6 +65,7 @@ export default function AllUsersTable() {
         disableRowSelectionOnClick
         disableMultipleRowSelection
       />
+      {confirmationDialogOpen && <ConfirmationDialog message={message} confirm={() => { deleteUser(userIdToDelete) }} />}
     </>
   )
 }
