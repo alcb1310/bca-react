2f3fd239493981962df6b43829f23287af74cb0c
Andres Court <andres@andrescourt.com>
2024-10-12 20:09:25 -0500

    feat: categories page

--- src/pages/parametros/categories/index.tsx
+++ src/pages/parametros/categories/index.tsx
@@ -1,9 +1,14 @@
+import AllCategoriesTable from "../../../components/settings/categories/AllCategoriesTable"
+import EditToolbar from "../../../components/table/headers/toolbar"
 import PageTitle from "../../../components/titles/PageTitle"
 
 export default function Categories() {
   return (
     <>
       <PageTitle title="Categorías" />
+
+      <EditToolbar title="Crear Categoría" onClick={() => {}}/>
+      <AllCategoriesTable />
     </>
   )
 }
