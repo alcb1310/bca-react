e173225195b71fd94e09f3275ed4fea9b5ce4e63
Andres Court <andres@andrescourt.com>
2024-10-15 23:23:25 -0500

    budget landing page

--- src/pages/transacciones/presupuesto/index.tsx
+++ src/pages/transacciones/presupuesto/index.tsx
@@ -1,9 +1,29 @@
+import { ChangeEvent, useState } from 'react'
+import { Stack, TextField } from '@mui/material'
+
+import EditToolbar from '../../../components/table/headers/toolbar'
 import PageTitle from '../../../components/titles/PageTitle'
+import AllBudgetsTable from '../../../components/parameters/budgets/AllBudgetsTable'
 
 export default function Presupuesto() {
+  const [search, setSearch] = useState<string>('')
+
   return (
     <>
       <PageTitle title='Presupuesto' />
+
+      <Stack spacing={3} direction='row'>
+        <EditToolbar title={'Agregar'} onClick={() => { }} />
+        <TextField
+          placeholder='Buscar'
+          size='small'
+          value={search}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}
+          className='w-full'
+        />
+      </Stack>
+
+      <AllBudgetsTable data={[]} />
     </>
   )
 }
