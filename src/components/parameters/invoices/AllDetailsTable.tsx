af82a822011aa5fad8dfe10b4a14aefdd3d556f9
Andres Court <andres@andrescourt.com>
2024-12-07 10:47:51 -0500

    FIX: remove unused dependencies

--- src/components/parameters/invoices/AllDetailsTable.tsx
+++ src/components/parameters/invoices/AllDetailsTable.tsx
@@ -2,103 +2,102 @@ import { DataGrid, GridActionsCellItem, GridColDef } from '@mui/x-data-grid'
 import { InvoiceDetailsResponseType } from '../../../types/invoiceDetails'
 import { DeleteOutlined } from '@mui/icons-material'
 import { useDeleteInvoiceDetailsMutation } from '../../../redux/api/bca-backend/transacciones/invoiceDetailsSlice'
-import SpentDetailsDrawer from '../../reports/SpentDetails'
 
 type AllDetailsTableProps = {
-  data: InvoiceDetailsResponseType[]
-  invoiceId: string
+    data: InvoiceDetailsResponseType[]
+    invoiceId: string
 }
 
 export default function AllDetailsTable({
-  data,
-  invoiceId,
+    data,
+    invoiceId,
 }: AllDetailsTableProps) {
-  const [deleteDetail] = useDeleteInvoiceDetailsMutation()
+    const [deleteDetail] = useDeleteInvoiceDetailsMutation()
 
-  const cols: GridColDef<InvoiceDetailsResponseType>[] = [
-    {
-      field: 'budget_item_code',
-      headerName: 'Codigo',
-      width: 120,
-    },
-    {
-      field: 'budget_item_name',
-      headerName: 'Nombre',
-      width: 500,
-    },
-    {
-      field: 'quantity',
-      headerName: 'Cantidad',
-      width: 150,
-      align: 'right',
-      valueFormatter: (params: number) => {
-        return params.toLocaleString('es-EC', {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2,
-        })
-      },
-    },
-    {
-      field: 'cost',
-      headerName: 'Costo',
-      width: 150,
-      align: 'right',
-      valueFormatter: (params: number) => {
-        return params.toLocaleString('es-EC', {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2,
-        })
-      },
-    },
-    {
-      field: 'total',
-      headerName: 'Total',
-      width: 150,
-      align: 'right',
-      valueFormatter: (params: number) => {
-        return params.toLocaleString('es-EC', {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2,
-        })
-      },
-    },
-    {
-      field: 'actions',
-      type: 'actions',
-      width: 10,
-      getActions: (params) => [
-        <GridActionsCellItem
-          icon={<DeleteOutlined color='error' />}
-          label='Borrar'
-          onClick={() =>
-            deleteDetail({
-              invoiceId,
-              detailId: params.row.budget_item_id,
-            })
-          }
-        />,
-      ],
-    },
-  ]
-  return (
-    <>
-      <DataGrid
-        rows={data}
-        columns={cols}
-        getRowId={(row) => row.id}
-        rowHeight={25}
-        disableColumnFilter
-        disableColumnResize
-        disableRowSelectionOnClick
-        disableMultipleRowSelection
-        sx={{ '&, [class^=MuiDataGrid]': { border: 'none' } }}
-        pagination
-        initialState={{
-          pagination: {
-            paginationModel: { pageSize: 25 },
-          },
-        }}
-      />
-    </>
-  )
+    const cols: GridColDef<InvoiceDetailsResponseType>[] = [
+        {
+            field: 'budget_item_code',
+            headerName: 'Codigo',
+            width: 120,
+        },
+        {
+            field: 'budget_item_name',
+            headerName: 'Nombre',
+            width: 500,
+        },
+        {
+            field: 'quantity',
+            headerName: 'Cantidad',
+            width: 150,
+            align: 'right',
+            valueFormatter: (params: number) => {
+                return params.toLocaleString('es-EC', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                })
+            },
+        },
+        {
+            field: 'cost',
+            headerName: 'Costo',
+            width: 150,
+            align: 'right',
+            valueFormatter: (params: number) => {
+                return params.toLocaleString('es-EC', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                })
+            },
+        },
+        {
+            field: 'total',
+            headerName: 'Total',
+            width: 150,
+            align: 'right',
+            valueFormatter: (params: number) => {
+                return params.toLocaleString('es-EC', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                })
+            },
+        },
+        {
+            field: 'actions',
+            type: 'actions',
+            width: 10,
+            getActions: (params) => [
+                <GridActionsCellItem
+                    icon={<DeleteOutlined color='error' />}
+                    label='Borrar'
+                    onClick={() =>
+                        deleteDetail({
+                            invoiceId,
+                            detailId: params.row.budget_item_id,
+                        })
+                    }
+                />,
+            ],
+        },
+    ]
+    return (
+        <>
+            <DataGrid
+                rows={data}
+                columns={cols}
+                getRowId={(row) => row.id}
+                rowHeight={25}
+                disableColumnFilter
+                disableColumnResize
+                disableRowSelectionOnClick
+                disableMultipleRowSelection
+                sx={{ '&, [class^=MuiDataGrid]': { border: 'none' } }}
+                pagination
+                initialState={{
+                    pagination: {
+                        paginationModel: { pageSize: 25 },
+                    },
+                }}
+            />
+        </>
+    )
 }
