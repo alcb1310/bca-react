f8f75f380b1f92c3ea8cff29499897cfb9983284
Andres Court <andres@andrescourt.com>
2025-01-19 21:48:41 -0500

    chore: bcaselect reformat file

--- src/components/input/BcaSelect.tsx
+++ src/components/input/BcaSelect.tsx
@@ -5,31 +5,31 @@ import { Controller } from 'react-hook-form'
 type BcaSelectProps = BcaField<SelectProps>
 
 export default function BcaSelect({
-  name,
-  control,
-  disabled,
-  children,
+    name,
+    control,
+    disabled,
+    children,
 }: BcaSelectProps) {
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field: { ref, ...fieldRest }, fieldState: { error } }) => (
-        <>
-          <NativeSelect
-            variant='filled'
-            disabled={disabled}
-            sx={{ '& .MuiInputBase-input': { px: 2 } }}
-            {...fieldRest}
-            error={!!error}
-          >
-            {children}
-          </NativeSelect>
-          <FormHelperText error={!!error} component='span' variant='standard'>
-            {error?.message}
-          </FormHelperText>
-        </>
-      )}
-    />
-  )
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({ field: { ref, ...fieldRest }, fieldState: { error } }) => (
+                <>
+                    <NativeSelect
+                        variant='filled'
+                        disabled={disabled}
+                        sx={{ '& .MuiInputBase-input': { px: 2 } }}
+                        {...fieldRest}
+                        error={!!error}
+                    >
+                        {children}
+                    </NativeSelect>
+                    <FormHelperText error={!!error} component='span' variant='standard'>
+                        {error?.message}
+                    </FormHelperText>
+                </>
+            )}
+        />
+    )
 }
