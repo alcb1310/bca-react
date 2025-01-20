8463cb42ada14015d7e9e56a9b5e44ba07bc2952
Andres Court <andres@andrescourt.com>
2025-01-19 20:38:10 -0500

    chore: Budget drawer reformat file

--- src/components/drawers/Transactions/BudgetDrawer.tsx
+++ src/components/drawers/Transactions/BudgetDrawer.tsx
@@ -11,148 +11,148 @@ import { useGetAllBudgetItemsQuery } from '../../../redux/api/bca-backend/parame
 import BcaTextField from '../../input/BcaTextField'
 import { DevTool } from '@hookform/devtools'
 import {
-  useCreateBudgetMutation,
-  useUpdateBudgetMutation,
+    useCreateBudgetMutation,
+    useUpdateBudgetMutation,
 } from '../../../redux/api/bca-backend/transacciones/budgetSlice'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 type BudgetDrawerProps = {
-  open: boolean
-  onClose: () => void
-  defaultValues: BudgetEditType
+    open: boolean
+    onClose: () => void
+    defaultValues: BudgetEditType
 }
 
 export default function BudgetDrawer({
-  open,
-  onClose,
-  defaultValues,
+    open,
+    onClose,
+    defaultValues,
 }: BudgetDrawerProps) {
-  const [conflictError, setConflictError] = useState<string>('')
-  const { control, reset, handleSubmit, setValue, getValues } =
-    useForm<BudgetEditType>({
-      defaultValues,
-      resolver: zodResolver(budgetEditSchema),
+    const [conflictError, setConflictError] = useState<string>('')
+    const { control, reset, handleSubmit, setValue, getValues } =
+        useForm<BudgetEditType>({
+            defaultValues,
+            resolver: zodResolver(budgetEditSchema),
+        })
+
+    const { data: projects } = useGetAllProjectsQuery({
+        active: true,
+    })
+    const { data: budgetItems } = useGetAllBudgetItemsQuery({
+        accum: false,
     })
 
-  const { data: projects } = useGetAllProjectsQuery({
-    active: true,
-  })
-  const { data: budgetItems } = useGetAllBudgetItemsQuery({
-    accum: false,
-  })
+    const [createBudget] = useCreateBudgetMutation()
+    const [updateBudget] = useUpdateBudgetMutation()
 
-  const [createBudget] = useCreateBudgetMutation()
-  const [updateBudget] = useUpdateBudgetMutation()
+    useEffect(() => {
+        reset(defaultValues)
+    }, [open])
 
-  useEffect(() => {
-    reset(defaultValues)
-  }, [open])
+    async function hadleSubmit(data: BudgetEditType) {
+        const costo = parseFloat(data.cost?.toString() || '0')
+        const cantidad = parseFloat(data.quantity?.toString() || '0')
 
-  async function hadleSubmit(data: BudgetEditType) {
-    const costo = parseFloat(data.cost?.toString() || '0')
-    const cantidad = parseFloat(data.quantity?.toString() || '0')
+        const dataToSave = {
+            project_id: data.project_id,
+            budget_item_id: data.budget_item_id,
+            quantity: cantidad,
+            cost: costo,
+            total: cantidad * costo,
+        }
 
-    const dataToSave = {
-      project_id: data.project_id,
-      budget_item_id: data.budget_item_id,
-      quantity: cantidad,
-      cost: costo,
-      total: cantidad * costo,
+        if (!defaultValues.project_id) {
+            const res = await createBudget(dataToSave)
+            if ('data' in res) {
+                onClose()
+                return
+            }
+
+            // @ts-expect-error data property is part of the res.error object
+            setConflictError(res.error.data.error)
+            return
+        }
+
+        const res = await updateBudget(dataToSave)
+        if ('data' in res) {
+            onClose()
+            return
+        }
+
+        // @ts-expect-error data property is part of the res.error object
+        setConflictError(res.error.data.erro)
     }
 
-    if (!defaultValues.project_id) {
-      const res = await createBudget(dataToSave)
-      if ('data' in res) {
-        onClose()
-        return
-      }
+    return (
+        <>
+            <BcaDrawer open={open} onClose={onClose}>
+                <DrawerTitle title='Proyectos' close={onClose} />
 
-      // @ts-expect-error data property is part of the res.error object
-      setConflictError(res.error.data.error)
-      return
-    }
+                <Box mt={2}>
+                    <form
+                        className='w-full flex flex-col gap-5'
+                        onSubmit={handleSubmit(hadleSubmit)}
+                    >
+                        {conflictError && (
+                            <Typography color='error'>{conflictError}</Typography>
+                        )}
+                        <BcaSelect control={control} name='project_id' label='Proyectos'>
+                            <option value=''>Seleccione un proyecto</option>
+                            {projects?.map((project) => (
+                                <option key={project.id} value={project.id}>
+                                    {project.name}
+                                </option>
+                            ))}
+                        </BcaSelect>
 
-    const res = await updateBudget(dataToSave)
-    if ('data' in res) {
-      onClose()
-      return
-    }
+                        <BcaSelect control={control} name='budget_item_id' label='Partida'>
+                            <option value=''>Seleccione una partida</option>
+                            {budgetItems?.map((budgetItem) => (
+                                <option key={budgetItem.id} value={budgetItem.id}>
+                                    {budgetItem.name}
+                                </option>
+                            ))}
+                        </BcaSelect>
 
-    // @ts-expect-error data property is part of the res.error object
-    setConflictError(res.error.data.erro)
-  }
+                        <BcaTextField
+                            name='quantity'
+                            label='Cantidad'
+                            control={control}
+                            onChange={(e) => {
+                                if (!isNaN(parseFloat(e.target.value))) {
+                                    setValue('quantity', parseFloat(e.target.value))
+                                    setValue(
+                                        'total',
+                                        getValues('cost') * parseFloat(e.target.value)
+                                    )
+                                } else {
+                                    console.log(e.target.value)
+                                }
+                            }}
+                        />
+                        <BcaTextField
+                            name='cost'
+                            label='Costo'
+                            control={control}
+                            onChange={(e) => {
+                                if (!isNaN(parseFloat(e.target.value))) {
+                                    setValue('cost', parseFloat(e.target.value))
+                                    setValue(
+                                        'total',
+                                        getValues('quantity') * parseFloat(e.target.value)
+                                    )
+                                }
+                            }}
+                        />
+                        <BcaTextField name='total' control={control} disabled />
 
-  return (
-    <>
-      <BcaDrawer open={open} onClose={onClose}>
-        <DrawerTitle title='Proyectos' close={onClose} />
-
-        <Box mt={2}>
-          <form
-            className='w-full flex flex-col gap-5'
-            onSubmit={handleSubmit(hadleSubmit)}
-          >
-            {conflictError && (
-              <Typography color='error'>{conflictError}</Typography>
-            )}
-            <BcaSelect control={control} name='project_id' label='Proyectos'>
-              <option value=''>Seleccione un proyecto</option>
-              {projects?.map((project) => (
-                <option key={project.id} value={project.id}>
-                  {project.name}
-                </option>
-              ))}
-            </BcaSelect>
-
-            <BcaSelect control={control} name='budget_item_id' label='Partida'>
-              <option value=''>Seleccione una partida</option>
-              {budgetItems?.map((budgetItem) => (
-                <option key={budgetItem.id} value={budgetItem.id}>
-                  {budgetItem.name}
-                </option>
-              ))}
-            </BcaSelect>
-
-            <BcaTextField
-              name='quantity'
-              label='Cantidad'
-              control={control}
-              onChange={(e) => {
-                if (!isNaN(parseFloat(e.target.value))) {
-                  setValue('quantity', parseFloat(e.target.value))
-                  setValue(
-                    'total',
-                    getValues('cost') * parseFloat(e.target.value)
-                  )
-                } else {
-                  console.log(e.target.value)
-                }
-              }}
-            />
-            <BcaTextField
-              name='cost'
-              label='Costo'
-              control={control}
-              onChange={(e) => {
-                if (!isNaN(parseFloat(e.target.value))) {
-                  setValue('cost', parseFloat(e.target.value))
-                  setValue(
-                    'total',
-                    getValues('quantity') * parseFloat(e.target.value)
-                  )
-                }
-              }}
-            />
-            <BcaTextField name='total' control={control} disabled />
-
-            <ButtonGroup
-              saveFunction={handleSubmit(hadleSubmit)}
-              cancelFunction={onClose}
-            />
-          </form>
-        </Box>
-      </BcaDrawer>
-      <DevTool control={control} />
-    </>
-  )
+                        <ButtonGroup
+                            saveFunction={handleSubmit(hadleSubmit)}
+                            cancelFunction={onClose}
+                        />
+                    </form>
+                </Box>
+            </BcaDrawer>
+            <DevTool control={control} />
+        </>
+    )
 }
