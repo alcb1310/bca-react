e00a9b988ec94a501fdb0f5dc3cafc2c108a9543
Andres Court <andres@andrescourt.com>
2025-01-19 21:52:55 -0500

    chore: BcaTextField reformat file

--- src/components/input/BcaTextField.tsx
+++ src/components/input/BcaTextField.tsx
@@ -5,42 +5,42 @@ import { Controller } from 'react-hook-form'
 type BcaTextFieldProps = BcaField<TextFieldProps>
 
 export default function BcaTextField({
-  name,
-  defaultValue,
-  control,
-  disabled,
-  ...rest
+    name,
+    defaultValue,
+    control,
+    disabled,
+    ...rest
 }: BcaTextFieldProps) {
-  return (
-    <>
-      <Controller
-        name={name}
-        control={control}
-        defaultValue={defaultValue}
-        render={({ field: { ref, ...fieldRest }, fieldState: { error } }) => (
-          <TextField
-            {...fieldRest}
-            inputRef={ref}
-            label={name}
-            error={!!error}
-            disabled={disabled}
-            size='small'
-            helperText={
-              <FormHelperText
-                error={!!error}
-                component='span'
-                variant='standard'
-              >
-                {error?.message}
-              </FormHelperText>
-            }
-            sx={{
-              width: '100%',
-            }}
-            {...rest}
-          />
-        )}
-      />
-    </>
-  )
+    return (
+        <>
+            <Controller
+                name={name}
+                control={control}
+                defaultValue={defaultValue}
+                render={({ field: { ref, ...fieldRest }, fieldState: { error } }) => (
+                    <TextField
+                        {...fieldRest}
+                        inputRef={ref}
+                        label={name}
+                        error={!!error}
+                        disabled={disabled}
+                        size='small'
+                        helperText={
+                            <FormHelperText
+                                error={!!error}
+                                component='span'
+                                variant='standard'
+                            >
+                                {error?.message}
+                            </FormHelperText>
+                        }
+                        sx={{
+                            width: '100%',
+                        }}
+                        {...rest}
+                    />
+                )}
+            />
+        </>
+    )
 }
