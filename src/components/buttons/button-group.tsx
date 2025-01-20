af304f421b8877a73dfd247a81570cb8df83fb2e
Andres Court <andres@andrescourt.com>
2025-01-19 21:46:42 -0500

    chore: button group reformat file

--- src/components/buttons/button-group.tsx
+++ src/components/buttons/button-group.tsx
@@ -2,36 +2,36 @@ import { CancelOutlined, SaveOutlined } from '@mui/icons-material'
 import { Button, Stack } from '@mui/material'
 
 type ButtonGroupProps = {
-  saveFunction: () => void
-  cancelFunction: () => void
+    saveFunction: () => void
+    cancelFunction: () => void
 }
 
 export default function ButtonGroup({
-  saveFunction,
-  cancelFunction,
+    saveFunction,
+    cancelFunction,
 }: ButtonGroupProps) {
-  return (
-    <Stack direction='row' justifyContent='space-between' alignItems='center'>
-      <Button
-        variant='contained'
-        startIcon={<SaveOutlined />}
-        color='primary'
-        onClick={saveFunction}
-        type='submit'
-        size='small'
-      >
-        Guardar
-      </Button>
+    return (
+        <Stack direction='row' justifyContent='space-between' alignItems='center'>
+            <Button
+                variant='contained'
+                startIcon={<SaveOutlined />}
+                color='primary'
+                onClick={saveFunction}
+                type='submit'
+                size='small'
+            >
+                Guardar
+            </Button>
 
-      <Button
-        variant='outlined'
-        startIcon={<CancelOutlined />}
-        color='primary'
-        onClick={cancelFunction}
-        size='small'
-      >
-        Cancelar
-      </Button>
-    </Stack>
-  )
+            <Button
+                variant='outlined'
+                startIcon={<CancelOutlined />}
+                color='primary'
+                onClick={cancelFunction}
+                size='small'
+            >
+                Cancelar
+            </Button>
+        </Stack>
+    )
 }
