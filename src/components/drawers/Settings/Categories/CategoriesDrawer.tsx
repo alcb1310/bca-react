9f210e61c2bd2ed2087d1061c2439776f2258dcc
Andres Court <andres@andrescourt.com>
2025-01-24 14:04:57 -0500

    chore(CategoriesDrawer): reformat file

--- src/components/drawers/Settings/Categories/CategoriesDrawer.tsx
+++ src/components/drawers/Settings/Categories/CategoriesDrawer.tsx
@@ -8,91 +8,91 @@ import { useForm } from 'react-hook-form'
 import { CategoryType } from '../../../../types/categories'
 import ButtonGroup from '../../../buttons/button-group'
 import {
-  useCreateCategoryMutation,
-  useUpdateCategoryMutation,
+    useCreateCategoryMutation,
+    useUpdateCategoryMutation,
 } from '../../../../redux/api/bca-backend/parametros/categoriesSlice'
 
 type CategoriesDrawerProps = {
-  open: boolean
-  onClose: () => void
-  defaultValues: CategoryType
+    open: boolean
+    onClose: () => void
+    defaultValues: CategoryType
 }
 
 export default function CategoriesDrawer({
-  open,
-  onClose,
-  defaultValues,
-}: CategoriesDrawerProps) {
-  const [conflictError, setConflictError] = useState<string>('')
-
-  const { control, reset, handleSubmit } = useForm<CategoryType>({
+    open,
+    onClose,
     defaultValues,
-  })
+}: CategoriesDrawerProps) {
+    const [conflictError, setConflictError] = useState<string>('')
 
-  const [createCategory] = useCreateCategoryMutation()
-  const [updateCategory] = useUpdateCategoryMutation()
+    const { control, reset, handleSubmit } = useForm<CategoryType>({
+        defaultValues,
+    })
 
-  useEffect(() => {
-    reset(defaultValues)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [open])
+    const [createCategory] = useCreateCategoryMutation()
+    const [updateCategory] = useUpdateCategoryMutation()
 
-  async function hadleSubmit(data: CategoryType) {
-    if (!defaultValues.id) {
-      const res = await createCategory(data)
-      if ('data' in res) {
-        onClose()
-        return
-      }
+    useEffect(() => {
+        reset(defaultValues)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [open])
 
-      if ('error' in res) {
-        // @ts-expect-error data property is part of the res.error object
-        setConflictError(res.error.data.error)
-        return
-      }
+    async function hadleSubmit(data: CategoryType) {
+        if (!defaultValues.id) {
+            const res = await createCategory(data)
+            if ('data' in res) {
+                onClose()
+                return
+            }
+
+            if ('error' in res) {
+                // @ts-expect-error data property is part of the res.error object
+                setConflictError(res.error.data.error)
+                return
+            }
+        }
+
+        const res = await updateCategory(data)
+        if ('data' in res) {
+            onClose()
+            return
+        }
+
+        if ('error' in res) {
+            // @ts-expect-error data property is part of the res.error object
+            setConflictError(res.error.data.error)
+            return
+        }
     }
 
-    const res = await updateCategory(data)
-    if ('data' in res) {
-      onClose()
-      return
-    }
+    return (
+        <BcaDrawer open={open} onClose={onClose}>
+            <DrawerTitle title='Crear Categorias' close={onClose} />
 
-    if ('error' in res) {
-      // @ts-expect-error data property is part of the res.error object
-      setConflictError(res.error.data.error)
-      return
-    }
-  }
+            <Box mt={2}>
+                <form
+                    className='w-full flex flex-col gap-5'
+                    onSubmit={handleSubmit(hadleSubmit)}
+                >
+                    {conflictError && (
+                        <Typography color='error' sx={{ fontSize: '0.85rem' }}>
+                            {conflictError}
+                        </Typography>
+                    )}
 
-  return (
-    <BcaDrawer open={open} onClose={onClose}>
-      <DrawerTitle title='Crear Categorias' close={onClose} />
+                    <BcaTextField
+                        name='name'
+                        label='Categoría'
+                        type='text'
+                        control={control}
+                    />
 
-      <Box mt={2}>
-        <form
-          className='w-full flex flex-col gap-5'
-          onSubmit={handleSubmit(hadleSubmit)}
-        >
-          {conflictError && (
-            <Typography color='error' sx={{ fontSize: '0.85rem' }}>
-              {conflictError}
-            </Typography>
-          )}
-
-          <BcaTextField
-            name='name'
-            label='Categoría'
-            type='text'
-            control={control}
-          />
-
-          <ButtonGroup
-            saveFunction={handleSubmit(hadleSubmit)}
-            cancelFunction={onClose}
-          />
-        </form>
-      </Box>
-    </BcaDrawer>
-  )
+                    <ButtonGroup
+                        saveFunction={handleSubmit(hadleSubmit)}
+                        cancelFunction={onClose}
+                    />
+                </form>
+            </Box>
+        </BcaDrawer>
+    )
 }
