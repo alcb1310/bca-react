80f1da27e431e76d82dfc39d9b462fcc50fc3cd2
Andres Court <andres@andrescourt.com>
2025-01-24 19:24:06 -0500

    chore(ProjectDrawer): Reformat the file

--- src/components/drawers/Settings/Projects/ProjectDrawer.tsx
+++ src/components/drawers/Settings/Projects/ProjectDrawer.tsx
@@ -10,93 +10,93 @@ import { projectSchema, ProjectType } from '../../../../types/project'
 import BcaTextField from '../../../input/BcaTextField'
 import ButtonGroup from '../../../buttons/button-group'
 import {
-  useCreateProjectMutation,
-  useUpdateProjectMutation,
+    useCreateProjectMutation,
+    useUpdateProjectMutation,
 } from '../../../../redux/api/bca-backend/parametros/projectsSlice'
 
 type ProjectDrawerProps = {
-  open: boolean
-  onClose: () => void
-  defaultValues: ProjectType
+    open: boolean
+    onClose: () => void
+    defaultValues: ProjectType
 }
 
 export default function ProjectDrawer({
-  open,
-  onClose,
-  defaultValues,
-}: ProjectDrawerProps) {
-  const [conflictError, setConflictError] = useState<string>('')
-  const { control, reset, handleSubmit } = useForm<ProjectType>({
+    open,
+    onClose,
     defaultValues,
-    resolver: zodResolver(projectSchema),
-  })
+}: ProjectDrawerProps) {
+    const [conflictError, setConflictError] = useState<string>('')
+    const { control, reset, handleSubmit } = useForm<ProjectType>({
+        defaultValues,
+        resolver: zodResolver(projectSchema),
+    })
 
-  const [createProject] = useCreateProjectMutation()
-  const [updateProject] = useUpdateProjectMutation()
+    const [createProject] = useCreateProjectMutation()
+    const [updateProject] = useUpdateProjectMutation()
 
-  useEffect(() => {
-    reset()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [open])
+    useEffect(() => {
+        reset()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [open])
 
-  async function hadleSubmit(data: ProjectType) {
-    setConflictError('')
-    data.gross_area = parseFloat(data.gross_area?.toString() || '0')
-    data.net_area = parseFloat(data.net_area?.toString() || '0')
+    async function hadleSubmit(data: ProjectType) {
+        setConflictError('')
+        data.gross_area = parseFloat(data.gross_area?.toString() || '0')
+        data.net_area = parseFloat(data.net_area?.toString() || '0')
 
-    if (!defaultValues.id) {
-      const res = await createProject(data)
-      if ('data' in res) {
-        onClose()
-        return
-      }
+        if (!defaultValues.id) {
+            const res = await createProject(data)
+            if ('data' in res) {
+                onClose()
+                return
+            }
 
-      // @ts-expect-error data property is part of the res.error object
-      setConflictError(res.error.data.error)
-      return
+            // @ts-expect-error data property is part of the res.error object
+            setConflictError(res.error.data.error)
+            return
+        }
+
+        const res = await updateProject(data)
+        if ('data' in res) {
+            onClose()
+            return
+        }
+
+        // @ts-expect-error data property is part of the res.error object
+        setConflictError(res.error.data.error)
     }
 
-    const res = await updateProject(data)
-    if ('data' in res) {
-      onClose()
-      return
-    }
+    return (
+        <BcaDrawer open={open} onClose={onClose}>
+            <DrawerTitle title='Proyectos' close={onClose} />
 
-    // @ts-expect-error data property is part of the res.error object
-    setConflictError(res.error.data.error)
-  }
+            <form
+                className='mt-5 flex flex-col gap-5'
+                onSubmit={handleSubmit(hadleSubmit)}
+            >
+                {conflictError && (
+                    <Typography color='error' variant='body2'>
+                        {conflictError}
+                    </Typography>
+                )}
+                <BcaTextField name='name' label='Nombre' control={control} />
+                <BcaTextField name='net_area' label='Area Bruta' control={control} />
+                <BcaTextField name='gross_area' label='Area Util' control={control} />
 
-  return (
-    <BcaDrawer open={open} onClose={onClose}>
-      <DrawerTitle title='Proyectos' close={onClose} />
+                <FormControlLabel
+                    name='is_active'
+                    labelPlacement='end'
+                    label='Activo'
+                    control={
+                        <RhfSwitch name='is_active' control={control} size='small' />
+                    }
+                />
 
-      <form
-        className='mt-5 flex flex-col gap-5'
-        onSubmit={handleSubmit(hadleSubmit)}
-      >
-        {conflictError && (
-          <Typography color='error' variant='body2'>
-            {conflictError}
-          </Typography>
-        )}
-        <BcaTextField name='name' label='Nombre' control={control} />
-        <BcaTextField name='net_area' label='Area Bruta' control={control} />
-        <BcaTextField name='gross_area' label='Area Util' control={control} />
-
-        <FormControlLabel
-          name='is_active'
-          labelPlacement='end'
-          label='Activo'
-          control={
-            <RhfSwitch name='is_active' control={control} size='small' />
-          }
-        />
-
-        <ButtonGroup
-          saveFunction={handleSubmit(hadleSubmit)}
-          cancelFunction={onClose}
-        />
-      </form>
-    </BcaDrawer>
-  )
+                <ButtonGroup
+                    saveFunction={handleSubmit(hadleSubmit)}
+                    cancelFunction={onClose}
+                />
+            </form>
+        </BcaDrawer>
+    )
 }
