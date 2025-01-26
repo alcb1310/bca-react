927959d1a66baf4512b0c7dca0884d57bb123874
Andres Court <andres@andrescourt.com>
2025-01-25 21:49:13 -0500

    test(UsersDrawer): should validate required fields

--- src/components/drawers/Users/UsersDrawer.cy.tsx
+++ src/components/drawers/Users/UsersDrawer.cy.tsx
@@ -3,7 +3,7 @@ import UsersDrawer from './UsersDrawer'
 
 describe('<UsersDrawer />', () => {
     describe('creating a user', () => {
-        it('should display all the fields', () => {
+        beforeEach(() => {
             cy.mount(
                 <TestAppWrapper>
                     <UsersDrawer
@@ -17,7 +17,9 @@ describe('<UsersDrawer />', () => {
                     />
                 </TestAppWrapper>
             )
+        })
 
+        it('should display all the fields', () => {
             cy.get('[data-testid="component.drawertitle.title"]')
                 .should('be.visible')
                 .should('have.text', 'Crear usuario')
@@ -41,6 +43,22 @@ describe('<UsersDrawer />', () => {
             cy.get('[data-testid="component.button.group.cancel"]')
                 .should('be.visible')
                 .should('have.text', 'Cancelar')
+        })
+
+        it('should validate all fields', () => {
+            cy.get('[data-testid="component.button.group.save"]').click()
+
+            cy.get('[data-testid="component.drawer.user.email.error"]')
+                .should('be.visible')
+                .should('have.text', 'Email es obligatorio')
+
+            cy.get('[data-testid="component.drawer.user.name.error"]')
+                .should('be.visible')
+                .should('have.text', 'Nombre es obligatorio')
+
+            cy.get('[data-testid="component.drawer.user.password.error"]')
+                .should('be.visible')
+                .should('have.text', 'Contraseña es obligatoria')
         })
     })
 })
