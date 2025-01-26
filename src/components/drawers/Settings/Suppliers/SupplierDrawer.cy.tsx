bf231f8acae92852326fb22326504a3aa3692220
Andres Court <andres@andrescourt.com>
2025-01-26 17:16:20 -0500

    test(SupplierDrawer): shuld display all field when editing

--- src/components/drawers/Settings/Suppliers/SupplierDrawer.cy.tsx
+++ src/components/drawers/Settings/Suppliers/SupplierDrawer.cy.tsx
@@ -130,4 +130,62 @@ describe('<SupplierDrawer />', () => {
             })
         })
     })
+
+    describe('Crear Proveedor', () => {
+        it('should display all values', () => {
+            cy.mount(
+                <TestAppWrapper>
+                    <SupplierDrawer
+                        open={true}
+                        onClose={() => { }}
+                        defaultValues={{
+                            id: '22877229-4c96-42a2-b3b8-7d68bbfee4d7',
+                            supplier_id: '1234567892001',
+                            name: 'Test Supplier',
+                            contact_name: 'Test Name',
+                            contact_email: 'test@test.com',
+                            contact_phone: '09-1234-5690',
+                        }}
+                    />
+                </TestAppWrapper>
+            )
+
+            cy.get('[data-testid="component.drawertitle.title"]')
+                .should('be.visible')
+                .should('have.text', 'Editar Proveedor')
+
+            cy.get('[data-testid="component.drawer.settings.supplier.supplier_id"]')
+                .find('input')
+                .should('be.visible')
+                .should('have.value', '1234567892001')
+
+            cy.get('[data-testid="component.drawer.settings.supplier.name"]')
+                .find('input')
+                .should('be.visible')
+                .should('have.value', 'Test Supplier')
+
+            cy.get('[data-testid="component.drawer.settings.supplier.contact_name"]')
+                .find('input')
+                .should('be.visible')
+                .should('have.value', 'Test Name')
+
+            cy.get('[data-testid="component.drawer.settings.supplier.contact_email"]')
+                .find('input')
+                .should('be.visible')
+                .should('have.value', 'test@test.com')
+
+            cy.get('[data-testid="component.drawer.settings.supplier.contact_phone"]')
+                .find('input')
+                .should('be.visible')
+                .should('have.value', '09-1234-5690')
+
+            cy.get('[data-testid="component.button.group.save"]')
+                .should('be.visible')
+                .should('have.text', 'Guardar')
+
+            cy.get('[data-testid="component.button.group.cancel"]')
+                .should('be.visible')
+                .should('have.text', 'Cancelar')
+        })
+    })
 })
