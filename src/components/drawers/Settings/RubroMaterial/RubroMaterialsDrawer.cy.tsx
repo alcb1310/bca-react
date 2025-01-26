d127b5efefeaf70dcf3b16872800b52e7e28d651
Andres Court <andres@andrescourt.com>
2025-01-26 17:00:18 -0500

    test(RubroMaterialsDrawer): should display all data when editing a rubro material

--- src/components/drawers/Settings/RubroMaterial/RubroMaterialsDrawer.cy.tsx
+++ src/components/drawers/Settings/RubroMaterial/RubroMaterialsDrawer.cy.tsx
@@ -114,4 +114,56 @@ describe('<RubroMaterialsDrawer />', () => {
             })
         })
     })
+
+    describe('Edit a Rubro Material', () => {
+        it('should display all the values', () => {
+            cy.intercept('GET', '**/parametros/materiales**', {
+                statusCode: 200,
+                fixture: 'parameters/materials/getAllMaterials.json',
+            }).as('materials')
+
+            cy.mount(
+                <TestAppWrapper>
+                    <RubroMaterialsDrawer
+                        open={true}
+                        onClose={() => { }}
+                        defaultValues={{
+                            item_id: '06aea29a-aa9e-4d33-9ccb-78b38833e33b',
+                            material_id: '11321ee2-274c-47ce-bb06-e8b131dbca12',
+                            quantity: 20.43,
+                        }}
+                    />
+                </TestAppWrapper>
+            )
+
+            cy.get('[data-testid="component.drawertitle.title"]')
+                .should('be.visible')
+                .should('have.text', 'Editar Material')
+
+            cy.get(
+                '[data-testid="component.drawer.settings.rubro.material.material"]'
+            )
+                .find('select')
+                .should('be.visible')
+                .should('be.disabled')
+                .invoke('val')
+                .should('eq', '11321ee2-274c-47ce-bb06-e8b131dbca12')
+
+            cy.get(
+                '[data-testid="component.drawer.settings.rubro.material.quantity"]'
+            )
+                .find('input')
+                .should('be.visible')
+                .invoke('val')
+                .should('eq', '20.43')
+
+            cy.get('[data-testid="component.button.group.save"]')
+                .should('be.visible')
+                .should('have.text', 'Guardar')
+
+            cy.get('[data-testid="component.button.group.cancel"]')
+                .should('be.visible')
+                .should('have.text', 'Cancelar')
+        })
+    })
 })
