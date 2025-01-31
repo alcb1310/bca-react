4b268ae3c0bd0de14374b29a7824b1c2f208dd00
Andres Court <andres@andrescourt.com>
2025-01-30 23:06:47 -0500

    test(items detail): display page with edit info

--- src/pages/parametros/items/detail/index.cy.tsx
+++ src/pages/parametros/items/detail/index.cy.tsx
@@ -3,6 +3,9 @@ import IndividualItem from '.'
 import { testStore } from '../../../../redux/testStore'
 import { MemoryRouter } from 'react-router-dom'
 
+const cols = ['Código', 'Nombre', 'Unidad', 'Cantidad']
+const title = ['code', 'name', 'unit', 'quantity']
+
 describe('<IndividualItem />', () => {
     describe('Create an item', () => {
         beforeEach(() => {
@@ -128,6 +131,74 @@ describe('<IndividualItem />', () => {
                     'not.be.visible'
                 )
             })
+        })
+    })
+
+    describe('update an item', () => {
+        it('should display the page', () => {
+            cy.intercept(
+                'GET',
+                '**/parametros/rubros/df344545-d20b-4e4c-97c6-6a8f65743abb',
+                {
+                    statusCode: 200,
+                    fixture: 'parameters/rubros/getOneRubro.json',
+                }
+            ).as('item')
+            cy.mount(
+                <Provider store={testStore}>
+                    <MemoryRouter
+                        initialEntries={[
+                            '/parametros/rubros/df344545-d20b-4e4c-97c6-6a8f65743abb',
+                        ]}
+                    >
+                        <IndividualItem />
+                    </MemoryRouter>
+                </Provider>
+            )
+            cy.get('[data-testid="page.parameters.item.detail.loading"]').should(
+                'be.visible'
+            )
+
+            cy.wait('@item')
+            cy.get('[data-testid="page.parameters.item.detail.loading"]').should(
+                'not.exist'
+            )
+            cy.get('[data-testid="component.pagetitle.title"]')
+                .should('be.visible')
+                .should('have.text', 'Editar Rubro')
+
+            cy.get('[data-testid="component.form.rubro.code"]')
+                .should('be.visible')
+                .find('input')
+                .should('have.value', 'h210')
+
+            cy.get('[data-testid="component.form.rubro.name"]')
+                .should('be.visible')
+                .find('input')
+                .should('have.value', 'Hormigon fc=210 kg/cm2')
+
+            cy.get('[data-testid="component.form.rubro.unit"]')
+                .should('be.visible')
+                .find('input')
+                .should('have.value', 'm3')
+
+            cy.get('[data-testid="component.button.group.save"]')
+                .should('be.visible')
+                .should('have.text', 'Guardar')
+
+            cy.get('[data-testid="component.button.group.cancel"]')
+                .should('be.visible')
+                .should('have.text', 'Cancelar')
+
+            cy.get('[data-testid="component.table.header.toolbar.main"]')
+                .should('be.visible')
+                .should('have.text', 'Agregar Material')
+
+            for (var i = 0; i < cols.length; i++) {
+                cy.get(`[data-field="${title[i]}"]`)
+                    .find('.MuiDataGrid-columnHeaderTitle')
+                    .should('have.text', cols[i])
+            }
         })
     })
 })
