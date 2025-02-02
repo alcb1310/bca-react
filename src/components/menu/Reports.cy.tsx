d808515c4adbff3dd5bdff38457520d211e9be16
Andres Court <andres@andrescourt.com>
2025-02-02 09:52:09 -0500

    test(reports menu): use custom cypress commands

--- src/components/menu/Reports.cy.tsx
+++ src/components/menu/Reports.cy.tsx
@@ -1,63 +1,57 @@
-import TestAppWrapper from '../wrappers/TestAppWraper'
 import ReportsMenu from './Reports'
 
 describe('<ReportesMenu />', () => {
     beforeEach(() => {
-        cy.mount(
-            <TestAppWrapper>
-                <ReportsMenu />
-            </TestAppWrapper>
-        )
+        cy.wrapper(<ReportsMenu />)
     })
 
     it('should display the reports menu', () => {
-        cy.get('[data-testid="menu.reports"]').should('have.text', 'Reportes')
-        cy.get('[data-testid="menu.reports.open-chevron"]').should('be.visible')
-        cy.get('[data-testid="menu.reports.closed-chevron"]').should('not.exist')
+        cy.getByTestId('menu.reports').should('have.text', 'Reportes')
+        cy.getByTestId('menu.reports.open-chevron').should('be.visible')
+        cy.getByTestId('menu.reports.closed-chevron').should('not.exist')
 
-        cy.get('[data-testid="menu.reports.actual"]').should('be.visible')
-        cy.get('[data-testid="menu.reports.actual"]').should('have.text', 'Actual')
+        cy.getByTestId('menu.reports.actual')
+            .should('be.visible')
+            .should('have.text', 'Actual')
 
-        cy.get('[data-testid="menu.reports.balance"]').should('be.visible')
-        cy.get('[data-testid="menu.reports.balance"]').should('have.text', 'Cuadre')
+        cy.getByTestId('menu.reports.balance')
+            .should('be.visible')
+            .should('have.text', 'Cuadre')
 
-        cy.get('[data-testid="menu.reports.spent"]').should('be.visible')
-        cy.get('[data-testid="menu.reports.spent"]').should(
+        cy.getByTestId('menu.reports.spent').should('be.visible')
+        cy.getByTestId('menu.reports.spent').should(
             'have.text',
             'Gastado por Partida'
         )
 
-        cy.get('[data-testid="menu.reports.historic"]').should('be.visible')
-        cy.get('[data-testid="menu.reports.historic"]').should(
-            'have.text',
-            'Historico'
-        )
+        cy.getByTestId('menu.reports.historic').should('be.visible')
+        cy.getByTestId('menu.reports.historic').should('have.text', 'Historico')
     })
 
     describe('toggle the menu', () => {
         beforeEach(() => {
-            cy.get('[data-testid="menu.reports"]').click()
+            cy.getByTestId('menu.reports').click()
         })
 
         it('should hide the menu when click the title', () => {
-            cy.get('[data-testid="menu.reports.closed-chevron"]').should('be.visible')
-            cy.get('[data-testid="menu.reports.open-chevron"]').should('not.exist')
+            cy.getByTestId('menu.reports.closed-chevron').should('be.visible')
+            cy.getByTestId('menu.reports.open-chevron').should('not.exist')
 
-            cy.get('[data-testid="menu.reports.actual"]').should('not.exist')
-            cy.get('[data-testid="menu.reports.balance"]').should('not.exist')
-            cy.get('[data-testid="menu.reports.spent"]').should('not.exist')
-            cy.get('[data-testid="menu.reports.historic"]').should('not.exist')
+            cy.getByTestId('menu.reports.actual').should('not.exist')
+            cy.getByTestId('menu.reports.balance').should('not.exist')
+            cy.getByTestId('menu.reports.spent').should('not.exist')
+            cy.getByTestId('menu.reports.historic').should('not.exist')
         })
 
         it('should show the menu when click the title', () => {
-            cy.get('[data-testid="menu.reports"]').click()
-            cy.get('[data-testid="menu.reports.open-chevron"]').should('be.visible')
-            cy.get('[data-testid="menu.reports.closed-chevron"]').should('not.exist')
+            cy.getByTestId('menu.reports').click()
+            cy.getByTestId('menu.reports.open-chevron').should('be.visible')
+            cy.getByTestId('menu.reports.closed-chevron').should('not.exist')
 
-            cy.get('[data-testid="menu.reports.actual"]').should('be.visible')
-            cy.get('[data-testid="menu.reports.balance"]').should('be.visible')
-            cy.get('[data-testid="menu.reports.spent"]').should('be.visible')
-            cy.get('[data-testid="menu.reports.historic"]').should('be.visible')
+            cy.getByTestId('menu.reports.actual').should('be.visible')
+            cy.getByTestId('menu.reports.balance').should('be.visible')
+            cy.getByTestId('menu.reports.spent').should('be.visible')
+            cy.getByTestId('menu.reports.historic').should('be.visible')
         })
     })
 })
