06b469958ba4e7cbd5533b6fa398cdfadacba2e0
Andres Court <andres@andrescourt.com>
2025-02-02 00:24:29 -0500

    test(projects): use the custom cypress commands

--- src/pages/parametros/projects/index.cy.tsx
+++ src/pages/parametros/projects/index.cy.tsx
@@ -1,5 +1,4 @@
 import Projects from '.'
-import TestAppWrapper from '../../../components/wrappers/TestAppWraper'
 
 const cols = ['Nombre', 'Area Bruta', 'Area Util', 'Activo']
 const title = ['name', 'net_area', 'gross_area', 'is_active']
@@ -10,31 +9,23 @@ describe('<Projects />', () => {
             statusCode: 200,
             fixture: 'parameters/projects/getAllProjects.json',
         }).as('projects')
-        cy.mount(
-            <TestAppWrapper>
-                <Projects />
-            </TestAppWrapper>
-        )
+        cy.wrapper(<Projects />)
     })
 
     it('should display the page', () => {
-        cy.get('[data-testid="page.parameters.projects.loading"]').should(
-            'be.visible'
-        )
+        cy.getByTestId('page.parameters.projects.loading').should('be.visible')
         cy.wait('@projects')
-        cy.get('[data-testid="page.parameters.projects.loading"]').should(
-            'not.exist'
-        )
+        cy.getByTestId('page.parameters.projects.loading').should('not.exist')
 
-        cy.get('[data-testid="component.pagetitle.title"]')
+        cy.getByTestId('component.pagetitle.title')
             .should('be.visible')
             .should('have.text', 'Proyectos')
 
-        cy.get('[data-testid="component.table.header.toolbar.main"]')
+        cy.getByTestId('component.table.header.toolbar.main')
             .should('be.visible')
             .should('have.text', 'Crear Proyecto')
 
-        cy.get('[data-testid="page.parameters.projects.search"]')
+        cy.getByTestId('page.parameters.projects.search')
             .should('be.visible')
             .find('label')
             .should('have.text', 'Buscar')
@@ -51,21 +42,21 @@ describe('<Projects />', () => {
     })
 
     it('should display the drawer in create mode', () => {
-        cy.get('[data-testid="component.drawer"]').should('not.exist')
+        cy.getByTestId('component.drawer').should('not.exist')
 
-        cy.get('[data-testid="component.table.header.toolbar.main"]').click()
-        cy.get('[data-testid="component.drawer"]').should('be.visible')
+        cy.getByTestId('component.table.header.toolbar.main').click()
+        cy.getByTestId('component.drawer').should('be.visible')
 
-        cy.get('[data-testid="component.drawertitle.title"]')
+        cy.getByTestId('component.drawertitle.title')
             .should('be.visible')
             .should('have.text', 'Crear Proyecto')
 
-        cy.get('[data-testid="component.button.group.cancel"]').click()
-        cy.get('[data-testid="component.drawer"]').should('not.exist')
+        cy.getByTestId('component.button.group.cancel').click()
+        cy.getByTestId('component.drawer').should('not.exist')
     })
 
     it('should display the drawer in edit mode', () => {
-        cy.get('[data-testid="component.drawer"]').should('not.exist')
+        cy.getByTestId('component.drawer').should('not.exist')
 
         for (var i = 0; i < cols.length; i++) {
             cy.get(`[data-field="${title[i]}"]`)
@@ -76,13 +67,13 @@ describe('<Projects />', () => {
         cy.get('[data-rowindex="2"]')
             .find('[data-testid="EditOutlinedIcon"]')
             .click()
-        cy.get('[data-testid="component.drawer"]').should('be.visible')
+        cy.getByTestId('component.drawer').should('be.visible')
 
-        cy.get('[data-testid="component.drawertitle.title"]')
+        cy.getByTestId('component.drawertitle.title')
             .should('be.visible')
             .should('have.text', 'Editar Proyecto')
 
-        cy.get('[data-testid="component.button.group.cancel"]').click()
-        cy.get('[data-testid="component.drawer"]').should('not.exist')
+        cy.getByTestId('component.button.group.cancel').click()
+        cy.getByTestId('component.drawer').should('not.exist')
     })
 })
