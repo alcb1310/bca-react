c246927b4ad521c864f8e2cc0135d9fc1309ec5a
Andres Court <andres@andrescourt.com>
2025-02-03 22:35:51 -0500

    chore(users): reformat file

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,43 +3,43 @@ import PageTitle from '../../components/titles/PageTitle'
 import { useMeQuery } from '../../redux/api/bca-backend/user/userSlice'
 
 export default function UsersHome() {
-  const { data, isLoading } = useMeQuery()
+    const { data, isLoading } = useMeQuery()
 
-  const componentLayout = (
-    <>
-      <Typography variant='body1' component='p'>
-        <Typography variant='body1' component='span' color='success'>
-          Nombre:&nbsp;
-        </Typography>
-        {data?.name}
-      </Typography>
+    const componentLayout = (
+        <>
+            <Typography variant='body1' component='p'>
+                <Typography variant='body1' component='span' color='success'>
+                    Nombre:&nbsp;
+                </Typography>
+                {data?.name}
+            </Typography>
 
-      <Typography variant='body1' component='p'>
-        <Typography variant='body1' component='span' color='success'>
-          Email:&nbsp;
-        </Typography>
-        {data?.email}
-      </Typography>
-      <br />
+            <Typography variant='body1' component='p'>
+                <Typography variant='body1' component='span' color='success'>
+                    Email:&nbsp;
+                </Typography>
+                {data?.email}
+            </Typography>
+            <br />
 
-      <Typography
-        variant='body1'
-        component='p'
-        sx={{
-          fontSize: '0.8rem',
-        }}
-      >
-        Para modificar el perfil, favor contactarse con el administrador
-      </Typography>
-    </>
-  )
+            <Typography
+                variant='body1'
+                component='p'
+                sx={{
+                    fontSize: '0.8rem',
+                }}
+            >
+                Para modificar el perfil, favor contactarse con el administrador
+            </Typography>
+        </>
+    )
 
-  return (
-    <>
-      <PageTitle title='Perfil' />
-      <Box sx={{ marginTop: 2 }}>
-        {isLoading ? <CircularProgress /> : componentLayout}
-      </Box>
-    </>
-  )
+    return (
+        <>
+            <PageTitle title='Perfil' />
+            <Box sx={{ marginTop: 2 }}>
+                {isLoading ? <CircularProgress /> : componentLayout}
+            </Box>
+        </>
+    )
 }
