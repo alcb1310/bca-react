9252f2d205f3fc0e0d26e39c30296a05f1446c8a
Andres Court <andres@andrescourt.com>
2025-02-06 07:42:25 -0500

    test(report historic): Should display the page

---
+++ src/pages/reportes/historic/idex.cy.tsx
@@ -0,0 +1,86 @@
+import Historic from '.'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
+const cols = [
+    'Codigo',
+    'Partida',
+    'Cant. Gastada',
+    'Total Gastado',
+    'Cant. Disponible',
+    'Costo Disponible',
+    'Total Disponible',
+    'Presupuesto Actualizado',
+]
+const title = [
+    'budget_item_code',
+    'budget_item_name',
+    'spent_quantity',
+    'spent_total',
+    'remaining_quantity',
+    'remaining_cost',
+    'remaining_total',
+    'updated_budget',
+]
+
+describe('<Historic />', () => {
+    it('should display the page', () => {
+        cy.intercept('GET', '**/parametros/proyectos?', {
+            statusCode: 200,
+            fixture: 'parameters/projects/getAllProjects.json',
+        }).as('projects')
+        cy.intercept('GET', '**/reportes/levels', {
+            statusCode: 200,
+            fixture: 'reports/levels/getAllLevels.json',
+        }).as('levels')
+        cy.intercept('GET', '**/reportes/historico?project_id=&level=&date=', {
+            statusCode: 204,
+        }).as('histoirc-load')
+
+        cy.wrapper(
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Historic />
+            </LocalizationProvider>
+        )
+
+        cy.getByTestId('pages.reports.historic.loading').should('be.visible')
+        cy.wait(['@projects', '@levels', '@histoirc-load'])
+        cy.getByTestId('pages.reports.historic.loading').should('not.exist')
+
+        cy.getByTestId('component.pagetitle.title')
+            .should('be.visible')
+            .should('have.text', 'Historico')
+
+        cy.getByTestId('pages.reports.historic.project')
+            .should('be.visible')
+            .find('select')
+            .should('not.have.value')
+
+        cy.getByTestId('pages.reports.historic.level')
+            .should('be.visible')
+            .find('select')
+            .should('not.have.value')
+
+        cy.getByTestId('pages.reports.historic.date')
+            .find('label')
+            .should('have.text', 'Fecha')
+            .should('be.visible')
+
+        cy.getByTestId('component.table.header.toolbar.main')
+            .should('be.visible')
+            .should('have.text', 'Generar')
+        cy.getByTestId('component.table.header.toolbar.export')
+            .should('be.visible')
+            .should('have.text', 'Exportar')
+
+        for (var i = 0; i < cols.length; i++) {
+            cy.get(`[data-field="${title[i]}"]`)
+                .find('.MuiDataGrid-columnHeaderTitle')
+                .should('have.text', cols[i])
+
+            cy.get(`[data-field="${title[i]}"]`)
+                .find('.MuiDataGrid-columnHeaderTitle')
+                .click()
+        }
+    })
+})
