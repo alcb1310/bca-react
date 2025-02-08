b85ea626251b5d3ec682d9c71e2d9ff393640c38
Andres Court <andres@andrescourt.com>
2025-02-08 11:00:55 -0500

    chore(UnauthenticatedLayout): reformat file

--- src/components/layouts/UnauthenticatedLayout.tsx
+++ src/components/layouts/UnauthenticatedLayout.tsx
@@ -2,20 +2,20 @@ import { Box } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 
 export default function UnauthenticatedLayout() {
-  return (
-    <>
-      <Box
-        component='main'
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          minHeight: '100vh',
-          alignItems: 'center',
-          bgcolor: 'background.default',
-        }}
-      >
-        <Outlet />
-      </Box>
-    </>
-  )
+    return (
+        <>
+            <Box
+                component='main'
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    minHeight: '100vh',
+                    alignItems: 'center',
+                    bgcolor: 'background.default',
+                }}
+            >
+                <Outlet />
+            </Box>
+        </>
+    )
 }
