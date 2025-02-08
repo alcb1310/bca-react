00272bbb1b1128d3f4148a65e3902dd0344ebd39
Andres Court <andres@andrescourt.com>
2025-02-08 10:38:11 -0500

    refactor(MaterialsDrawer): use path alias

--- src/components/drawers/Settings/Materials/MaterialsDrawer.tsx
+++ src/components/drawers/Settings/Materials/MaterialsDrawer.tsx
@@ -3,17 +3,17 @@ import { useForm } from 'react-hook-form'
 import { CircularProgress, Typography } from '@mui/material'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-import { materialSchema, MaterialType } from '../../../../types/materials'
-import ButtonGroup from '../../../buttons/button-group'
-import DrawerTitle from '../../../titles/DrawerTitle'
-import BcaDrawer from '../../BcaDrawer/BcaDrawer'
-import BcaTextField from '../../../input/BcaTextField'
-import BcaSelect from '../../../input/BcaSelect'
-import { useGetAllCategoriesQuery } from '../../../../redux/api/bca-backend/parametros/categoriesSlice'
+import { materialSchema, MaterialType } from '~types/materials'
+import ButtonGroup from '~components/buttons/button-group'
+import DrawerTitle from '~components/titles/DrawerTitle'
+import BcaDrawer from '~components/drawers/BcaDrawer/BcaDrawer'
+import BcaTextField from '~components/input/BcaTextField'
+import BcaSelect from '~components/input/BcaSelect'
+import { useGetAllCategoriesQuery } from '~redux/api/bca-backend/parametros/categoriesSlice'
 import {
     useCreateMaterialMutation,
     useUpdateMaterialMutation,
-} from '../../../../redux/api/bca-backend/parametros/materialsSlice'
+} from '~redux/api/bca-backend/parametros/materialsSlice'
 
 type MaterialsDrawerProps = {
     open: boolean
