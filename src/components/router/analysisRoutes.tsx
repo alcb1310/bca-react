acb7663c7e30affcf4e2929cd66b99efa06f9adb
Andres Court <andres@andrescourt.com>
2025-02-08 11:10:51 -0500

    refactor(analysisRoutes): use path alias

--- src/components/router/analysisRoutes.tsx
+++ src/components/router/analysisRoutes.tsx
@@ -1,22 +1,23 @@
 import { RouteObject } from 'react-router-dom'
-import Analysis from '../../pages/analysis'
-import Quantity from '../../pages/analysis/quantity'
-import AnalysisReport from '../../pages/analysis/analysis'
+
+import Analysis from '~/pages/analysis'
+import Quantity from '~/pages/analysis/quantity'
+import AnalysisReport from '~/pages/analysis/analysis'
 
 export const analysisRoutes: RouteObject = {
-  path: 'analisis',
-  children: [
-    {
-      index: true,
-      element: <Analysis />,
-    },
-    {
-      path: 'cantidad',
-      element: <Quantity />,
-    },
-    {
-      path: 'analisis',
-      element: <AnalysisReport />,
-    },
-  ],
+    path: 'analisis',
+    children: [
+        {
+            index: true,
+            element: <Analysis />,
+        },
+        {
+            path: 'cantidad',
+            element: <Quantity />,
+        },
+        {
+            path: 'analisis',
+            element: <AnalysisReport />,
+        },
+    ],
 }
