d19507080efae821ec986d04f00358b4458212aa
Andres Court <andres@andrescourt.com>
2025-02-08 11:58:25 -0500

    refactor(suppliers): use path alias

--- src/pages/parametros/suppliers/index.tsx
+++ src/pages/parametros/suppliers/index.tsx
@@ -1,10 +1,11 @@
-import { CircularProgress, Grid2, TextField } from '@mui/material'
-import AllSuppliersTable from '../../../components/settings/suppliers/AllSuppliersTable'
-import PageTitle from '../../../components/titles/PageTitle'
-import { useGetAllSuppliersQuery } from '../../../redux/api/bca-backend/parametros/supplierSlice'
 import { useState } from 'react'
-import EditToolbar from '../../../components/table/headers/toolbar'
-import SupplierDrawer from '../../../components/drawers/Settings/Suppliers/SupplierDrawer'
+import { CircularProgress, Grid2, TextField } from '@mui/material'
+
+import AllSuppliersTable from '~components/settings/suppliers/AllSuppliersTable'
+import PageTitle from '~components/titles/PageTitle'
+import { useGetAllSuppliersQuery } from '~redux/api/bca-backend/parametros/supplierSlice'
+import EditToolbar from '~components/table/headers/toolbar'
+import SupplierDrawer from '~components/drawers/Settings/Suppliers/SupplierDrawer'
 
 export default function Suppliers() {
     const [query, setQuery] = useState<string>('')
