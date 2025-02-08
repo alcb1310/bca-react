61c0b992e658e2a9d1f2c46d4d59cfe46f15739e
Andres Court <andres@andrescourt.com>
2025-02-08 11:00:29 -0500

    refactor(AuthenticatedLayout): use path alias

--- src/components/layouts/AuthenticatedLayout.tsx
+++ src/components/layouts/AuthenticatedLayout.tsx
@@ -1,36 +1,37 @@
-import { Box } from '@mui/material'
-import Sidebar from '../sidebar/SideBar'
-import ApplicationBar from '../appbar/AppBar'
 import { Outlet } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import Sidebar from '~components/sidebar/SideBar'
+import ApplicationBar from '~components/appbar/AppBar'
 
 function AuthenticatedLayout() {
-  return (
-    <Box display='flex' width='100%' height='100%'>
-      <Sidebar />
-      <Box
-        flexGrow={1}
-        height='100%'
-        display='flex'
-        flexDirection='column'
-        sx={{ overflowX: 'auto' }}
-      >
-        <ApplicationBar />
+    return (
+        <Box display='flex' width='100%' height='100%'>
+            <Sidebar />
+            <Box
+                flexGrow={1}
+                height='100%'
+                display='flex'
+                flexDirection='column'
+                sx={{ overflowX: 'auto' }}
+            >
+                <ApplicationBar />
 
-        <Box
-          component='main'
-          flexGrow={1}
-          display='flex'
-          flexDirection='column'
-          sx={{ overflowY: 'auto', scrollBehavior: 'smooth' }}
-          id='main'
-          px={3}
-          py={1}
-        >
-          <Outlet />
+                <Box
+                    component='main'
+                    flexGrow={1}
+                    display='flex'
+                    flexDirection='column'
+                    sx={{ overflowY: 'auto', scrollBehavior: 'smooth' }}
+                    id='main'
+                    px={3}
+                    py={1}
+                >
+                    <Outlet />
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  )
+    )
 }
 
 export default AuthenticatedLayout
