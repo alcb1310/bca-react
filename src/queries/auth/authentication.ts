e52e1502b7ea061e629a4b90bfc5bf7dfb2fed0d
Andres Court <andres@andrescourt.com>
2025-05-18 17:07:54 -0500

    feat(query): login query

---
+++ src/queries/auth/authentication.ts
@@ -0,0 +1,28 @@
+import type { LoginInput } from '~/types/login'
+import type { UserResponse } from '~types/user'
+
+const url = import.meta.env.VITE_BACKEND_SERVER
+
+if (!url) throw new Error('VITE_BACKEND_SERVER environment variable not set')
+
+type loginResponse = {
+  token: string
+  user: UserResponse
+}
+
+export async function useLoginMutation(user: LoginInput) {
+  const response = await fetch(`${url}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+
+  if (!response.ok) {
+    const err = await response.json()
+    throw new Error(err.error)
+  }
+
+  return (await response.json()) as loginResponse
+}
