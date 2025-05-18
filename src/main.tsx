0860229fdb98ce9b2d666d9ba99fe691ced4b11b
Andres Court <andres@andrescourt.com>
2025-05-18 10:17:01 -0500

    lint(main): lint the main.tsx file according to biome

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import AppWrapper from "./components/wrappers/AppWrapper.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import AppWrapper from './components/wrappers/AppWrapper.tsx'
 
-import "./index.css";
+import './index.css'
 
-const root = document.getElementById("root");
-if (!root) throw new Error("Couldn't find the root element");
+const root = document.getElementById('root')
+if (!root) throw new Error("Couldn't find the root element")
 
 createRoot(root).render(
   <StrictMode>
     <AppWrapper />
   </StrictMode>,
-);
+)
