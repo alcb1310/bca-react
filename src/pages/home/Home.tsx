0207904db977c5ba399e73829cfa3f7a7b692dde
Andres Court <andres@andrescourt.com>
2025-05-18 10:56:10 -0500

    feat(home): use react-query to fetch data for the login screen

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,30 +1,36 @@
 import { CircularProgress, Typography } from '@mui/material'
-
-import { useMeQuery } from '~redux/api/bca-backend/user/userSlice'
+import { useQuery } from '@tanstack/react-query'
+import { meQuery } from '~/queries/user/user'
+import { useAppSelector } from '~/redux/hooks'
 
 export default function Home() {
-    const { data, isLoading } = useMeQuery()
-    return (
-        <>
-            {isLoading ? (
-                <CircularProgress data-testid='pages.home.spinner' />
-            ) : (
-                <Typography
-                    data-testid='pages.home.welcome'
-                    variant='body1'
-                    component='p'
-                >
-                    Bienvenido&nbsp;
-                    <Typography
-                        data-testid='pages.home.username'
-                        variant='body1'
-                        component='span'
-                        color='success'
-                    >
-                        {data?.name}
-                    </Typography>
-                </Typography>
-            )}
-        </>
-    )
+  const token = useAppSelector((state) => state.login.token)
+  const { data, isFetching } = useQuery({
+    queryKey: ['users', 'me'],
+    queryFn: () => meQuery({ token }),
+  })
+
+  return (
+    <>
+      {isFetching ? (
+        <CircularProgress data-testid='pages.home.spinner' />
+      ) : (
+        <Typography
+          data-testid='pages.home.welcome'
+          variant='body1'
+          component='p'
+        >
+          Bienvenido&nbsp;
+          <Typography
+            data-testid='pages.home.username'
+            variant='body1'
+            component='span'
+            color='success'
+          >
+            {data?.name}
+          </Typography>
+        </Typography>
+      )}
+    </>
+  )
 }
