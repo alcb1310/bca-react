82418d588e0b1f1966dbd45bbb151c7cd2c01b84
Andres Court <andres@andrescourt.com>
2025-05-18 17:09:19 -0500

    test(wrapper): added react-query to the cypress wrappers

--- cypress/support/wrappers.tsx
+++ cypress/support/wrappers.tsx
@@ -2,19 +2,26 @@ import { mount } from "cypress/react";
 import { Provider } from "react-redux";
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import { testStore } from "../../src/redux/testStore.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 Cypress.Commands.add("wrapper", (children) => {
-	return mount(
-		<Provider store={testStore}>
-			<BrowserRouter>{children}</BrowserRouter>
-		</Provider>,
-	);
+  return mount(
+    <Provider store={testStore}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </QueryClientProvider>
+    </Provider>,
+  );
 });
 
 Cypress.Commands.add("pageWrapper", (children, routes) => {
-	return mount(
-		<Provider store={testStore}>
-			<MemoryRouter initialEntries={routes}>{children}</MemoryRouter>
-		</Provider>,
-	);
+  return mount(
+    <Provider store={testStore}>
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter initialEntries={routes}>{children}</MemoryRouter>
+      </QueryClientProvider>
+    </Provider>,
+  );
 });
