fed71827e4f98a4840d5db6b586b22a697809a19
Andres Court <andres@andrescourt.com>
2025-05-18 10:01:32 -0500

    chore(tanstack-query): installed the tanstack-query library & setup to work
    npm install @tanstack/react-query

--- src/components/wrappers/AppWrapper.tsx
+++ src/components/wrappers/AppWrapper.tsx
@@ -1,21 +1,26 @@
-import { Provider } from 'react-redux'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { CssBaseline } from '@mui/material'
+import { Provider } from "react-redux";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { store } from '~redux/store'
-import Theme from '~components/theme/Theme'
-import AppRouter from '~components/router/Router'
+import { store } from "~redux/store";
+import Theme from "~components/theme/Theme";
+import AppRouter from "~components/router/Router";
+
+const queryClient = new QueryClient();
 
 export default function AppWrapper() {
-    return (
-        <Provider store={store}>
-            <Theme>
-                <CssBaseline />
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <AppRouter />
-                </LocalizationProvider>
-            </Theme>
-        </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Theme>
+          <CssBaseline />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <AppRouter />
+          </LocalizationProvider>
+        </Theme>
+      </QueryClientProvider>
+    </Provider>
+  );
 }
