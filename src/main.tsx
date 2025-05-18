54d17bd698b029e23fcfc2adb60e1dab856cab28
Andres Court <andres@andrescourt.com>
2025-05-18 10:04:20 -0500

    fix(main): throw error if root div is not found

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import AppWrapper from './components/wrappers/AppWrapper.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AppWrapper from "./components/wrappers/AppWrapper.tsx";
 
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+if (!root) throw new Error("Couldn't find the root element");
+
+createRoot(root).render(
   <StrictMode>
     <AppWrapper />
-  </StrictMode>
-)
+  </StrictMode>,
+);
