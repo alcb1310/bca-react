6d12c195b6b21cd59e5a257d1c93eab5bf248be7
Andres Court <andres@andrescourt.com>
2025-05-18 20:58:28 -0500

    refactor(react-query): use react-query to update user

--- src/components/drawers/Users/UsersDrawer.tsx
+++ src/components/drawers/Users/UsersDrawer.tsx
@@ -1,3 +1,4 @@
+// TODO: update user not working
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Box, Typography } from '@mui/material'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
@@ -5,10 +6,12 @@ import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import BcaTextField from '~/components/input/BcaTextField/BcaTextField'
 import DrawerTitle from '~/components/titles/DrawerTitle/DrawerTitle'
-import { useCreateUserMutation } from '~/queries/user/user'
+import {
+  useCreateUserMutation,
+  useUpdateUserMutation,
+} from '~/queries/user/user'
 import { useAppSelector } from '~/redux/hooks'
 import ButtonGroup from '~components/buttons/button-group'
-import { useUpdateUserMutation } from '~redux/api/bca-backend/user/userSlice'
 import {
   type UserCreate,
   type UserResponse,
@@ -16,6 +19,7 @@ import {
   userResponseSchema,
 } from '~types/user'
 import BcaDrawer from '../BcaDrawer/BcaDrawer'
+import { DevTool } from '@hookform/devtools'
 
 type UsersDrawerProps = {
   open: boolean
@@ -41,7 +45,17 @@ export default function UsersDrawer({
       setConflictError(error.message)
     },
   })
-  const [updateUser] = useUpdateUserMutation()
+  const { mutate: updateUser } = useMutation({
+    mutationFn: useUpdateUserMutation,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+      onClose()
+      reset()
+    },
+    onError: (error) => {
+      setConflictError(error.message)
+    },
+  })
   const [conflictError, setConflictError] = useState<string>('')
 
   const resolver =
@@ -56,79 +70,76 @@ export default function UsersDrawer({
     resolver,
   })
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: should run when open changes
   useEffect(() => {
     setConflictError('')
     reset(userData)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userData])
+  }, [userData, reset])
 
   async function hadleSubmit(data: UserCreate | UserResponse) {
+    console.log('saving', data)
     setConflictError('')
     if ('password' in data) {
       createUser({ token, user: data })
     } else {
-      const res = await updateUser(data)
-      if ('data' in res) {
-        onClose()
-        reset()
-        return
-      }
+      updateUser({ token, user: data })
     }
   }
 
   return (
-    <BcaDrawer open={open} onClose={onClose}>
-      <DrawerTitle
-        title={'password' in userData ? 'Crear usuario' : 'Editar usuario'}
-        close={onClose}
-      />
+    <>
+      <BcaDrawer open={open} onClose={onClose}>
+        <DrawerTitle
+          title={'password' in userData ? 'Crear usuario' : 'Editar usuario'}
+          close={onClose}
+        />
 
-      <Box mt={2}>
-        <form
-          className='w-full flex flex-col gap-5'
-          onSubmit={handleSubmit(hadleSubmit)}
-        >
-          {conflictError && (
-            <Typography color='error' variant='body2'>
-              {conflictError}
-            </Typography>
-          )}
+        <Box mt={2}>
+          <form
+            className='w-full flex flex-col gap-5'
+            onSubmit={handleSubmit(hadleSubmit)}
+          >
+            {conflictError && (
+              <Typography color='error' variant='body2'>
+                {conflictError}
+              </Typography>
+            )}
 
-          <BcaTextField
-            name='email'
-            datatestid='component.drawer.user.email'
-            label='Email'
-            type='email'
-            control={control}
-            disabled={'id' in userData}
-          />
-
-          <BcaTextField
-            name='name'
-            datatestid='component.drawer.user.name'
-            label='Nombre'
-            type='text'
-            control={control}
-          />
-
-          {'password' in userData && (
             <BcaTextField
-              name='password'
-              datatestid='component.drawer.user.password'
-              type='password'
-              label='Contraseña'
+              name='email'
+              datatestid='component.drawer.user.email'
+              label='Email'
+              type='email'
+              control={control}
               disabled={'id' in userData}
+            />
+
+            <BcaTextField
+              name='name'
+              datatestid='component.drawer.user.name'
+              label='Nombre'
+              type='text'
               control={control}
             />
-          )}
 
-          <ButtonGroup
-            saveFunction={handleSubmit(hadleSubmit)}
-            cancelFunction={onClose}
-          />
-        </form>
-      </Box>
-    </BcaDrawer>
+            {'password' in userData && (
+              <BcaTextField
+                name='password'
+                datatestid='component.drawer.user.password'
+                type='password'
+                label='Contraseña'
+                disabled={'id' in userData}
+                control={control}
+              />
+            )}
+
+            <ButtonGroup
+              saveFunction={handleSubmit(hadleSubmit)}
+              cancelFunction={onClose}
+            />
+          </form>
+        </Box>
+        <DevTool control={control} />
+      </BcaDrawer>
+    </>
   )
 }
