8433b25b9f8c61c37da00dae9c72f8e966658223
Andres Court <andres@andrescourt.com>
2025-05-20 18:14:58 -0500

    test(Home): fix flaky test

--- src/pages/home/Home.cy.tsx
+++ src/pages/home/Home.cy.tsx
@@ -3,7 +3,7 @@ import Home from './Home'
 describe('<Home />', () => {
   it('should display the home page', () => {
     // cy.intercept must be called BEFORE mounting the component
-    cy.intercept('GET', '**/users/me', { fixture: 'users/me.json' }).as(
+    cy.intercept('GET', 'api/v1/users/me', { fixture: 'users/me.json' }).as(
       'currentUser',
     )
 
