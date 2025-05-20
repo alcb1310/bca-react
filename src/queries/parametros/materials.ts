295da8bb9d612de504b006df6eb46ae9b5f4d675
Andres Court <andres@andrescourt.com>
2025-05-19 21:50:54 -0500

    feat(queries): added the creaet material query

--- src/queries/parametros/materials.ts
+++ src/queries/parametros/materials.ts
@@ -20,3 +20,24 @@ export async function useGetAllMaterialsQuery({
 
   return (await response.json()) as MaterialType[]
 }
+
+export async function useCreateMaterialMutation({
+  token,
+  material,
+}: Readonly<{ token: string; material: MaterialType }>) {
+  const response = await fetch(`${url}/parametros/materiales`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(material),
+  })
+
+  if (!response.ok) {
+    const err = await response.json()
+    throw new Error(err.error)
+  }
+
+  return (await response.json()) as MaterialType[]
+}
