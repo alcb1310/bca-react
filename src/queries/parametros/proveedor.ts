30816a9f0a0d3f54fa692271a0b482a919d13ec7
Andres Court <andres@andrescourt.com>
2025-05-23 13:14:58 -0500

    feat(queries): added the get all supplier query

---
+++ src/queries/parametros/proveedor.ts
@@ -0,0 +1,26 @@
+import type { SupplierType } from '~/types/supplier'
+
+const url = import.meta.env.VITE_BACKEND_SERVER
+
+export async function useGetAllSuppliersQuery({
+  token,
+  search,
+}: Readonly<{ token: string; search?: string }>) {
+  const params = new URLSearchParams()
+  if (search) params.append('search', search)
+
+  const response = await fetch(`${url}/parametros/proveedores?${params}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    const err = await response.json()
+    throw new Error(err.error)
+  }
+
+  return (await response.json()) as SupplierType[]
+}
