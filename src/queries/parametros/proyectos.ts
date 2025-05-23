83fca130cdcf53efa177414fa1188b265ca57008
Andres Court <andres@andrescourt.com>
2025-05-23 12:11:14 -0500

    feat(queries): added the get all projects query

---
+++ src/queries/parametros/proyectos.ts
@@ -0,0 +1,28 @@
+import type { ProjectType } from '~/types/project'
+
+const url = import.meta.env.VITE_BACKEND_SERVER
+
+export async function useGetAllProjectsQuery({
+  token,
+  query,
+  active,
+}: Readonly<{ token: string; query?: string; active?: boolean }>) {
+  const params = new URLSearchParams()
+  if (query) params.append('query', query)
+  if (active !== undefined) params.append('active', active ? 'true' : 'false')
+
+  const response = await fetch(`${url}/parametros/proyectos?${params}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    const err = await response.json()
+    throw new Error(err.error)
+  }
+
+  return (await response.json()) as ProjectType[]
+}
