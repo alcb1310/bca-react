448bc9e691c39ddeee96c87490578aa0796ee856
Andres Court <andres@andrescourt.com>
2025-05-24 08:47:44 -0500

    feat(queries): added the get one rubro query

--- src/queries/parametros/rubros.ts
+++ src/queries/parametros/rubros.ts
@@ -20,3 +20,23 @@ export async function useGetAllRubrosQuery({
 
   return (await response.json()) as RubrosType[]
 }
+
+export async function useGetOneRubroQuery({
+  token,
+  id,
+}: Readonly<{ token: string; id: string }>) {
+  const response = await fetch(`${url}/parametros/rubros/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    const err = await response.json()
+    throw new Error(err.error)
+  }
+
+  return (await response.json()) as RubrosType
+}
