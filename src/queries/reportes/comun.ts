adc0138c06fed266a15ff74f7bf5382b8f708d8e
Andres Court <andres@andrescourt.com>
2025-05-28 08:13:57 -0500

    feat(queries): added the get all levels query

--- src/queries/reportes/comun.ts
+++ src/queries/reportes/comun.ts
@@ -1,3 +1,5 @@
+import type { BudgetResponseType } from '~/types/budget'
+
 const url = import.meta.env.VITE_BACKEND_SERVER
 
 type LevelType = {
@@ -22,4 +24,36 @@ export async function useGetAllLevelsQuery({
   }
 
   return (await response.json()) as LevelType[]
+}
+
+export async function useGetAllHistoricQuery({
+  token,
+  project_id,
+  level,
+  date,
+}: Readonly<{
+  token: string
+  project_id: string
+  level: string
+  date: string
+}>) {
+  const params = new URLSearchParams()
+  params.append('project_id', project_id)
+  params.append('level', level)
+  params.append('date', date)
+
+  const response = await fetch(`${url}/reportes/historico?${params}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    const err = await response.json()
+    throw new Error(err.error)
+  }
+
+  return (await response.json()) as BudgetResponseType[]
 }
