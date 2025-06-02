1bed1219b65cb73bb9b1f2f802a927258fc42fa6
Andres Court <andres@andrescourt.com>
2025-06-01 20:30:28 -0500

    feat(react-query): setup react-query as context of react-router

--- src/components/wrappers/AppWrapper.tsx
+++ src/components/wrappers/AppWrapper.tsx
@@ -3,7 +3,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { Toaster } from 'sonner'
 import { routeTree } from '~/routeTree.gen'
 import Theme from '~components/theme/Theme'
@@ -12,20 +12,24 @@ const queryClient = new QueryClient()
 
 export const router = createRouter({
   routeTree,
+  context: { queryClient },
+  defaultPreload: 'intent',
 })
 
 export default function AppWrapper() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Theme>
-        <CssBaseline />
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <RouterProvider router={router} />
-          <Toaster />
-        </LocalizationProvider>
-      </Theme>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Theme>
+          <CssBaseline />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <RouterProvider router={router} />
+            <Toaster />
+          </LocalizationProvider>
+        </Theme>
 
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
   )
 }
