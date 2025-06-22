fab3c473d30a0df4cffb91f16636ceb492c0cdbe
Andres Court <andres@andrescourt.com>
2025-06-21 19:03:06 -0500

    feat(vite): setup the react-router vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
