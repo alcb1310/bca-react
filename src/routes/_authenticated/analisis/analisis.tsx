f4006bdea21f5375f8bf21bbab250c247b8be8ca
Andres Court <andres@andrescourt.com>
2025-06-24 21:54:17 -0500

    refactor(analisis): display the analisis routes

--- src/routes/_authenticated/analisis/analisis.tsx
+++ src/routes/_authenticated/analisis/analisis.tsx
@@ -1,9 +1,6 @@
+import AnalysisReport from '@/pages/analysis/analysis'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/analisis/analisis')({
-  component: RouteComponent,
+  component: AnalysisReport,
 })
-
-function RouteComponent() {
-  return <div>Hello "/_authenticated/analisis/analisis"!</div>
-}
--- src/routes/_authenticated/analisis/cantidad.tsx
+++ src/routes/_authenticated/analisis/cantidad.tsx
@@ -1,9 +1,6 @@
+import Quantity from '@/pages/analysis/quantity'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/analisis/cantidad')({
-  component: RouteComponent,
+  component: Quantity,
 })
-
-function RouteComponent() {
-  return <div>Hello "/_authenticated/analisis/cantidad"!</div>
-}
