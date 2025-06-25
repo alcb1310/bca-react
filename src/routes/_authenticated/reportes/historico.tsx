8c4bec87c6836eb47fecfa6cc784810c2f5e7f37
Andres Court <andres@andrescourt.com>
2025-06-24 20:42:51 -0500

    refactor(reports): display all the reports pages

--- src/routes/_authenticated/reportes/historico.tsx
+++ src/routes/_authenticated/reportes/historico.tsx
@@ -1,9 +1,6 @@
+import Historic from '@/pages/reportes/historic'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/reportes/historico')({
-  component: RouteComponent,
+  component: Historic,
 })
-
-function RouteComponent() {
-  return <div>Hello "/_authenticated/reportes/historico"!</div>
-}
