cca8cfd40da2af476c193e6583e78817ec8eef3e
Andres Court <andres@andrescourt.com>
2025-06-28 09:46:55 -0500

    feat(tanstack query): setup tanstack query to work in the application

--- src/components/wrappers/AppWrapper.tsx
+++ src/components/wrappers/AppWrapper.tsx
@@ -7,10 +7,16 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { Provider } from 'react-redux'
 import { routeTree } from '@/routeTree.gen'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export const router = createRouter({
   routeTree,
   defaultPreload: 'intent',
+  context: { queryClient },
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
 })
 
 declare module '@tanstack/react-router' {
@@ -21,14 +27,16 @@ declare module '@tanstack/react-router' {
 
 export default function AppWrapper() {
   return (
-    <Provider store={store}>
-      <Theme>
-        <CssBaseline />
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <RouterProvider router={router} />
-          <Toaster />
-        </LocalizationProvider>
-      </Theme>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Theme>
+          <CssBaseline />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <RouterProvider router={router} />
+            <Toaster />
+          </LocalizationProvider>
+        </Theme>
+      </Provider>
+    </QueryClientProvider>
   )
 }
