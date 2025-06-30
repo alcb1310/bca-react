d3c5f034f572c2118457b2d93f545630a5a489d7
Andres Court <andres@andrescourt.com>
2025-06-30 17:41:55 -0500

    feat(tanstack query): add tanstack as part of the router context

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,9 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import type { QueryClient } from '@tanstack/react-query'
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
   component: RootComponent,
 })
 
