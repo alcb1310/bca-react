349f982015b5cda10a8d9e19af190cdf22442cf4
Andres Court <andres@andrescourt.com>
2025-09-19 16:37:45 -0500

    formatting the application bar

--- src/components/appbar/AppBar.tsx
+++ src/components/appbar/AppBar.tsx
@@ -1,54 +1,54 @@
 import { LogoutOutlined } from '@mui/icons-material'
 import {
-  AppBar,
-  Box,
-  IconButton,
-  Stack,
-  Toolbar,
-  Tooltip,
-  Typography,
+    AppBar,
+    Box,
+    IconButton,
+    Stack,
+    Toolbar,
+    Tooltip,
+    Typography,
 } from '@mui/material'
 import { logout } from '@/redux/features/login/loginSlice'
 import { useAppDispatch } from '@/redux/hooks'
 import UserMenu from '@/components/menu/User'
 
 export default function ApplicationBar() {
-  const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch()
 
-  function handleLogout() {
-    dispatch(logout())
-    window.location.reload()
-  }
+    function handleLogout() {
+        dispatch(logout())
+        window.location.reload()
+    }
 
-  return (
-    <AppBar position='sticky'>
-      <Toolbar>
-        <Typography
-          data-testid='title'
-          variant='h6'
-          noWrap
-          component='h6'
-          textTransform='uppercase'
-          fontWeight={700}
-          sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-        >
-          Sistema Control Prespuestario
-        </Typography>
+    return (
+        <AppBar position='sticky'>
+            <Toolbar>
+                <Typography
+                    data-testid='title'
+                    variant='h6'
+                    noWrap
+                    component='h6'
+                    textTransform='uppercase'
+                    fontWeight={700}
+                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                >
+                    Sistema Control Prespuestario
+                </Typography>
 
-        <Box sx={{ flexGrow: 1 }} />
-        <Stack direction='row' spacing={2}>
-          <UserMenu />
-          <Tooltip title='Cerrar sesión'>
-            <IconButton
-              size='large'
-              onClick={handleLogout}
-              sx={{ color: 'white' }}
-            >
-              <LogoutOutlined data-testid='logout-icon' />
-            </IconButton>
-          </Tooltip>
-        </Stack>
-      </Toolbar>
-    </AppBar>
-  )
+                <Box sx={{ flexGrow: 1 }} />
+                <Stack direction='row' spacing={2}>
+                    <UserMenu />
+                    <Tooltip title='Cerrar sesión'>
+                        <IconButton
+                            size='large'
+                            onClick={handleLogout}
+                            sx={{ color: 'white' }}
+                        >
+                            <LogoutOutlined data-testid='logout-icon' />
+                        </IconButton>
+                    </Tooltip>
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    )
 }
