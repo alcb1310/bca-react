078190b14330018ce908902316457941cdd96775
Andres Court <andres@andrescourt.com>
2025-09-19 16:39:30 -0500

    reformat file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import AppWrapper from '@/components/wrappers/AppWrapper.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppWrapper from '@/components/wrappers/AppWrapper.tsx'
 
 import './index.css'
 
