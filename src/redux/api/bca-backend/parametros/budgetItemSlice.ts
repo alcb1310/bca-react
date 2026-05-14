4a6bc09d708c0f9a82e138825a75058eb42691d8
Andres Court <andres@andrescourt.com>
2026-05-13 20:27:01 -0500

    refactor remove unused getAllBudgetItemsByAccumulate function

--- src/redux/api/bca-backend/parametros/budgetItemSlice.ts
+++ src/redux/api/bca-backend/parametros/budgetItemSlice.ts
@@ -2,78 +2,58 @@ import type { BudgetItem, BudgetItemResponse } from '@/types/partidas'
 import { bcaApiSlice } from '../bcaSlice'
 
 const partidasEndpoints = bcaApiSlice.injectEndpoints({
-  overrideExisting: true,
+    overrideExisting: true,
 
-  endpoints: (builder) => ({
-    getAllBudgetItems: builder.query<
-      BudgetItemResponse[],
-      {
-        query?: string
-        accum?: boolean
-      }
-    >({
-      query(body) {
-        return {
-          url: '/parametros/partidas',
-          method: 'GET',
-          params: {
-            query: body.query,
-            accum: body.accum,
-          },
-        }
-      },
+    endpoints: (builder) => ({
+        getAllBudgetItems: builder.query<
+            BudgetItemResponse[],
+            {
+                query?: string
+                accum?: boolean
+            }
+        >({
+            query(body) {
+                return {
+                    url: '/parametros/partidas',
+                    method: 'GET',
+                    params: {
+                        query: body.query,
+                        accum: body.accum,
+                    },
+                }
+            },
 
-      providesTags: ['partidas'],
+            providesTags: ['partidas'],
+        }),
+
+        createBudgetItem: builder.mutation<BudgetItem, BudgetItem>({
+            query(body) {
+                return {
+                    url: '/parametros/partidas',
+                    method: 'POST',
+                    body,
+                }
+            },
+
+            invalidatesTags: ['partidas'],
+        }),
+
+        updateBudgetItem: builder.mutation<BudgetItem, BudgetItem>({
+            query(body) {
+                return {
+                    url: `/parametros/partidas/${body.id}`,
+                    method: 'PUT',
+                    body,
+                }
+            },
+
+            invalidatesTags: ['partidas'],
+        }),
     }),
-
-    getAllBudgetItemsByAccumulate: builder.query<
-      BudgetItem[],
-      {
-        accumulate: boolean
-      }
-    >({
-      query(body) {
-        return {
-          url: '/parametros/partidas',
-          method: 'GET',
-          params: {
-            accumulate: body.accumulate,
-          },
-        }
-      },
-
-      providesTags: ['partidas'],
-    }),
-
-    createBudgetItem: builder.mutation<BudgetItem, BudgetItem>({
-      query(body) {
-        return {
-          url: '/parametros/partidas',
-          method: 'POST',
-          body,
-        }
-      },
-
-      invalidatesTags: ['partidas'],
-    }),
-
-    updateBudgetItem: builder.mutation<BudgetItem, BudgetItem>({
-      query(body) {
-        return {
-          url: `/parametros/partidas/${body.id}`,
-          method: 'PUT',
-          body,
-        }
-      },
-
-      invalidatesTags: ['partidas'],
-    }),
-  }),
 })
 
 export const {
-  useGetAllBudgetItemsQuery,
-  useGetAllBudgetItemsByAccumulateQuery,
-  useCreateBudgetItemMutation,
-  useUpdateBudgetItemMutation,
+    useGetAllBudgetItemsQuery,
+    useCreateBudgetItemMutation,
+    useUpdateBudgetItemMutation,
 } = partidasEndpoints
