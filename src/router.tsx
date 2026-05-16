b384abe707af8df7418366d539b316ceafe6950e
Andres Court <andres@andrescourt.com>
2026-05-16 16:09:29 -0500

    setup tanstack/react-query to be used in the application

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { QueryClient } from '@tanstack/react-query'
 
 export function getRouter() {
+    const queryClient = new QueryClient()
+
     const router = createRouter({
         routeTree,
         scrollRestoration: true,
+        context: { queryClient },
+        defaultPreload: 'intent',
     })
 
     return router
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,33 +1,34 @@
-// src/routes/__root.tsx
 /// <reference types="vite/client" />
-import type { ReactNode } from 'react'
+import type { QueryClient } from '@tanstack/react-query'
 import {
-    Outlet,
-    createRootRoute,
     HeadContent,
+    Outlet,
     Scripts,
+    createRootRouteWithContext,
 } from '@tanstack/react-router'
-
+import type { ReactNode } from 'react'
 import appCss from '../styles.css?url'
 
-export const Route = createRootRoute({
-    head: () => ({
-        meta: [
-            {
-                charSet: 'utf-8',
-            },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-            },
-            {
-                title: 'BCA',
-            },
-        ],
-        links: [{ rel: 'stylesheet', href: appCss }],
-    }),
-    component: RootComponent,
-})
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+    {
+        head: () => ({
+            meta: [
+                {
+                    charSet: 'utf-8',
+                },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+                {
+                    title: 'BCA',
+                },
+            ],
+            links: [{ rel: 'stylesheet', href: appCss }],
+        }),
+        component: RootComponent,
+    },
+)
 
 function RootComponent() {
     return (
