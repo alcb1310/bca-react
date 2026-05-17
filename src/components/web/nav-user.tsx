78ee8c42e311170adc2f7a61ac95a9ed236b9654
Andres Court <andres@andrescourt.com>
2026-05-17 17:09:13 -0500

    make nav-bar more beautiful

--- src/components/web/nav-user.tsx
+++ src/components/web/nav-user.tsx
@@ -39,19 +39,22 @@ export function NavUser({ user }: NavUserProps) {
 					<DropdownMenuTrigger asChild>
 						<SidebarMenuButton
 							size={"lg"}
-							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground bg-primary text-primary-foreground/80"
 						>
 							<div className="grid flex-1 text-left text-sm leading-tight">
 								<span className="truncate text-lg font-medium">
 									{user.name}
 								</span>
-								<span className="truncate text-xs">{user.email}</span>
+								<span className="truncate text-xs font-extralight">
+									{user.email}
+								</span>
 							</div>
 							<ChevronsUpDown className="ml-auto size-4" />
 						</SidebarMenuButton>
 					</DropdownMenuTrigger>
 					<DropdownMenuContent
 						className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+						side="right"
 						align="end"
 						sideOffset={4}
 					>
