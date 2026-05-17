efb34ec1d0a8391f51ffaeb768385539b718b096
Andres Court <andres@andrescourt.com>
2026-05-17 14:36:13 -0500

    use the information in the auth store to pass the user's queries

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -1,16 +1,16 @@
-import { store } from '@/redux/store'
+import { authStore } from '@/store/auth'
 import type { UserCreate, UserResponse } from '@/types/user'
 
 const URL = import.meta.env.VITE_BACKEND_SERVER
 
 export async function Me() {
-    const state = store.getState()
+    const token = authStore.get().token
 
     const response = await fetch(`${URL}/users/me`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${state.login.token}`,
+            Authorization: `Bearer ${token}`,
         },
     })
 
@@ -23,13 +23,13 @@ export async function Me() {
 }
 
 export async function GetAllUsers() {
-    const state = store.getState()
+    const token = authStore.get().token
 
     const response = await fetch(`${URL}/users`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${state.login.token}`,
+            Authorization: `Bearer ${token}`,
         },
     })
 
@@ -42,13 +42,13 @@ export async function GetAllUsers() {
 }
 
 export async function CreateUser({ data }: { data: UserCreate }) {
-    const state = store.getState()
+    const token = authStore.get().token
 
     const response = await fetch(`${URL}/users`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${state.login.token}`,
+            Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(data),
     })
@@ -62,13 +62,13 @@ export async function CreateUser({ data }: { data: UserCreate }) {
 }
 
 export async function DeleteUser(id: string) {
-    const state = store.getState()
+    const token = authStore.get().token
 
     const response = await fetch(`${URL}/users/${id}`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${state.login.token}`,
+            Authorization: `Bearer ${token}`,
         },
     })
 
@@ -81,13 +81,13 @@ export async function DeleteUser(id: string) {
 }
 
 export async function UpdateUser({ data }: { data: UserResponse }) {
-    const state = store.getState()
+    const token = authStore.get().token
 
     const response = await fetch(`${URL}/users/${data.id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${state.login.token}`,
+            Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(data),
     })
@@ -101,13 +101,13 @@ export async function UpdateUser({ data }: { data: UserResponse }) {
 }
 
 export async function UpdatePassword({ data }: { data: { password: string } }) {
-    const state = store.getState()
+    const token = authStore.get().token
 
     const response = await fetch(`${URL}/users/password`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${state.login.token}`,
+            Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(data),
     })
