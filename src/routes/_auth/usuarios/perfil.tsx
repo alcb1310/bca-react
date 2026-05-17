65ba331236f84b63d53cd6667dce89c43dcc720b
Andres Court <andres@andrescourt.com>
2026-05-17 16:50:42 -0500

    user's profile page

--- src/routes/_auth/usuarios/perfil.tsx
+++ src/routes/_auth/usuarios/perfil.tsx
@@ -1,9 +1,40 @@
+import { Spinner } from "@/components/ui/spinner";
+import PageTitle from "@/components/web/pageTitle";
+import { Me } from "@/queries/users";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth/usuarios/perfil")({
 	component: RouteComponent,
+	beforeLoad: ({ context: { queryClient } }) => {
+		queryClient.ensureQueryData({ queryKey: ["me"], queryFn: () => Me() });
+	},
 });
 
 function RouteComponent() {
-	return <div>Hello "/_auth/usuarios/perfil"!</div>;
+	const { data, isLoading } = useSuspenseQuery({
+		queryKey: ["me"],
+		queryFn: () => Me(),
+	});
+
+	return (
+		<div>
+			<PageTitle title="Perfil" />
+
+			{isLoading && <Spinner />}
+
+			<p>
+				<strong>Nombre:</strong>{" "}
+				<span className="text-chart-3">{data?.name}</span>
+			</p>
+			<p className="mb-5">
+				<strong>Email:</strong>
+				<span className="text-chart-3">{data?.email}</span>
+			</p>
+
+			<p className="text-xs font-extralight">
+				Para modificar el perfil, favor contactarse con el administrador
+			</p>
+		</div>
+	);
 }
