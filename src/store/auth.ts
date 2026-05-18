9953ee112ca8f1c7d53020e509a5e9256805fbd3
Andres Court <andres@andrescourt.com>
2026-05-18 17:44:41 -0500

    remove legacy redux files not longer used

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -7,3 +7,35 @@ export const authStore = new Store<{
 	user: null,
 	token: "",
 });
+//
+// const cookie =
+// 	document.cookie
+// 		.split(";")
+// 		.find((cookie) => cookie.includes("BCA-TOKEN"))
+// 		?.split("=")[1] || "";
+//
+// const initialState: LoginType = {
+// 	isLoggedIn: !!cookie,
+// 	token: cookie,
+// };
+//
+// export const loginSlice = createSlice({
+// 	name: "login",
+// 	initialState,
+// 	reducers: {
+// 		login: (state, user: PayloadAction<string>) => {
+// 			state.isLoggedIn = true;
+// 			state.token = user.payload;
+//
+// 			document.cookie = `BCA-TOKEN=${user.payload}; expires=1 day; path=/`;
+// 		},
+//
+// 		logout: (state) => {
+// 			state.isLoggedIn = false;
+// 			state.token = "";
+//
+// 			document.cookie =
+// 				"BCA-TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;";
+// 		},
+// 	},
+// });
