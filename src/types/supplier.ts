cca54130d0db597dcb0d5257b0cec88776e12bf7
Andres Court <andres@andrescourt.com>
2026-05-19 11:28:39 -0500

    supplier schema use what the backend responds like

--- src/types/supplier.ts
+++ src/types/supplier.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
 
+export const optionalStringSchema = z.object({
+	String: z.string().nullable(),
+	Valid: z.boolean(),
+});
+
 export const supplierSchema = z.object({
 	id: z.string().uuid().optional(),
 	supplier_id: z
@@ -7,9 +12,9 @@ export const supplierSchema = z.object({
 		.min(1, "Ruc del proveedor es obligatorio")
 		.min(10, "Ruc inválido"),
 	name: z.string().min(1, "Nombre es obligatorio"),
-	contact_name: z.string().optional(),
-	contact_phone: z.string().optional(),
-	contact_email: z.string().optional(),
+	contact_name: optionalStringSchema,
+	contact_phone: optionalStringSchema,
+	contact_email: optionalStringSchema,
 });
 
 export type SupplierType = z.infer<typeof supplierSchema>;
