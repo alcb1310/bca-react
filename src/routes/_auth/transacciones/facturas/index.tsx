cf4f116992b6444d439554c470be550fbf3b3f65
Andres Court <andres@andrescourt.com>
2026-05-20 11:14:20 -0500

    list all invoices page

--- src/routes/_auth/transacciones/facturas/index.tsx
+++ src/routes/_auth/transacciones/facturas/index.tsx
@@ -1,9 +1,80 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import { PlusIcon } from 'lucide-react'
+import { buttonVariants } from '@/components/ui/button'
+import { DataTable } from '@/components/ui/data-table'
+import { Spinner } from '@/components/ui/spinner'
+import PageTitle from '@/components/web/pageTitle'
+import { GetAllInvoices } from '@/queries/transacciones/invoice'
+import type { InvoiceResponseType } from '@/types/invoice'
 
 export const Route = createFileRoute('/_auth/transacciones/facturas/')({
 	component: RouteComponent,
+	loader: ({ context: { queryClient } }) => {
+		queryClient.prefetchQuery({
+			queryKey: ['facturas'],
+			queryFn: () => GetAllInvoices(),
+		})
+	},
 })
 
 function RouteComponent() {
-	return <div>Hello "/_auth/transacciones/facturas"!</div>
+	const { data, isLoading } = useSuspenseQuery({
+		queryKey: ['facturas'],
+		queryFn: () => GetAllInvoices(),
+	})
+
+	const columns: ColumnDef<InvoiceResponseType>[] = [
+		{
+			accessorKey: 'invoice_date',
+			header: 'Fecha',
+			cell: ({ row }) => {
+				const dt = new Date(row.original.invoice_date)
+				return dt.toLocaleDateString('es-EC', {
+					year: 'numeric',
+					month: '2-digit',
+					day: '2-digit',
+				})
+			},
+		},
+		{
+			accessorKey: 'project.name',
+			header: 'Proyecto',
+		},
+		{
+			accessorKey: 'supplier.name',
+			header: 'Proveedor',
+		},
+		{
+			accessorKey: 'invoice_number',
+			header: 'N° Factura',
+		},
+		{
+			accessorKey: 'invoice_total',
+			header: 'Total',
+			cell: ({ row }) => {
+				return row.original.invoice_total.toLocaleString('es-EC', {
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2,
+				})
+			},
+		},
+	]
+
+	return (
+		<div>
+			<PageTitle title='Facturas' />
+			<Link
+				to='/transacciones/facturas/crear'
+				className={`my-3 ${buttonVariants({ variant: 'default' })}`}
+			>
+				<PlusIcon size={16} />
+				Crear Factura
+			</Link>
+			{isLoading && <Spinner />}
+
+			<DataTable columns={columns} data={data} />
+		</div>
+	)
 }
