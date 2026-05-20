4a46286078eed2d6dca37f7dbc5b29544b04de10
Andres Court <andres@andrescourt.com>
2026-05-19 21:28:51 -0500

    use tanstack store to use login info in rubros queries

--- src/queries/parametros/rubros.ts
+++ src/queries/parametros/rubros.ts
@@ -1,16 +1,16 @@
-import { store } from '@/redux/store'
+import { authStore } from '@/store/auth'
 import type { RubrosType } from '@/types/rubros'
 
 const URL = import.meta.env.VITE_BACKEND_SERVER
 
 export async function GetAllRubros() {
-	const state = store.getState()
+	const token = authStore.state.token
 
 	const response = await fetch(`${URL}/parametros/rubros`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Bearer ${state.login.token}`,
+			Authorization: `Bearer ${token}`,
 		},
 	})
 
@@ -24,13 +24,13 @@ export async function GetAllRubros() {
 }
 
 export async function GetOneRubro(id: string) {
-	const state = store.getState()
+	const token = authStore.state.token
 
 	const response = await fetch(`${URL}/parametros/rubros/${id}`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Bearer ${state.login.token}`,
+			Authorization: `Bearer ${token}`,
 		},
 	})
 
@@ -44,13 +44,13 @@ export async function GetOneRubro(id: string) {
 }
 
 export async function CreateRubro({ data }: { data: RubrosType }) {
-	const state = store.getState()
+	const token = authStore.state.token
 
 	const response = await fetch(`${URL}/parametros/rubros`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Bearer ${state.login.token}`,
+			Authorization: `Bearer ${token}`,
 		},
 		body: JSON.stringify(data),
 	})
@@ -65,13 +65,13 @@ export async function CreateRubro({ data }: { data: RubrosType }) {
 }
 
 export async function UpdateRubro({ data }: { data: RubrosType }) {
-	const state = store.getState()
+	const token = authStore.state.token
 
 	const response = await fetch(`${URL}/parametros/rubros/${data.id}`, {
 		method: 'PUT',
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Bearer ${state.login.token}`,
+			Authorization: `Bearer ${token}`,
 		},
 		body: JSON.stringify(data),
 	})
