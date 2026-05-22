869df02e861cdd3b86051c348ea2af06ef5adcc1
Andres Court <andres@andrescourt.com>
2026-05-22 17:24:49 -0500

    fix closure syntax error

--- src/queries/transacciones/closure.ts
+++ src/queries/transacciones/closure.ts
@@ -6,23 +6,23 @@ const URL = import.meta.env.VITE_BACKEND_SERVER
 const cookieName = 'BCA-TOKEN'
 
 export const CreateClosure = createServerFn({ method: 'POST' })
-    .inputValidator((data: CierreTypes) => data)
-    .handler(async ({ data }) => {
-        const token = getCookie(cookieName)
+	.inputValidator((data: CierreTypes) => data)
+	.handler(async ({ data }) => {
+		const token = getCookie(cookieName)
 
-        const response = await fetch(`${URL}/transacciones/cierre`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(data),
-        })
+		const response = await fetch(`${URL}/transacciones/cierre`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify(data),
+		})
 
-        if (!response.ok) {
-            const error = await response.json()
-            throw new Error(error.error)
-        }
+		if (!response.ok) {
+			const error = await response.json()
+			throw new Error(error.error)
+		}
 
-        return
-    }
+		return
+	})
